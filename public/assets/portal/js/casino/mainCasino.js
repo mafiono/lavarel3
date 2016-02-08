ea12821da06ffeab00606184df0a80668c6334f7
Miguel Teixeira <miguel.teixeira@programmer.net>
2016-02-08 18:17:47 +0000

    Added: Casino category menu css

--- public/assets/portal/js/casino/mainCasino.js
+++ public/assets/portal/js/casino/mainCasino.js
@@ -1,6 +1,6 @@
 $(function() {
     $("#featured-carousel, #cards-carousel").owlCarousel({
-        items : 4,
+        items : 3,
         itemsScaleUp : false,
         pagination: false,
         navigation: false,
@@ -23,4 +23,17 @@ $(function() {
         cardsCarousel.next();
     });
 
+    $("#allMenuItem").click(function() {
+        $("#allMenuItem").addClass("casino-box-menuItemSelected");
+        $("#featuredMenuItem").removeClass("casino-box-menuItemSelected");
+        $("#allContainer").removeClass("hidden");
+        $("#featuredGamesContainer").addClass("hidden");
+    });
+
+    $("#featuredMenuItem").click(function() {
+        $("#allMenuItem").removeClass("casino-box-menuItemSelected");
+        $("#featuredMenuItem").addClass("casino-box-menuItemSelected");
+        $("#allContainer").addClass("hidden");
+        $("#featuredGamesContainer").removeClass("hidden");
+    });
 });
--- resources/views/portal/casino/casino.blade.php
+++ resources/views/portal/casino/casino.blade.php
@@ -9,90 +9,139 @@
 
 @section('content')
 <div class="casino-container">
-    <div class="casino-container-header">
-        <div class="acenter">
-            <button class="casino-button">Casino</button>
-            <button class="casino-button">Casino ao vivo</button>
-            <button class="casino-button">Promoções</button>
+    <div class="casino-container-menu">
+        <div id="allMenuItem" class="casino-box-menuItem casino-box-menuItemSelected">
+            <span class="casino-text-menuItemIcon casino-text-menuItemIconSelected">
+                <i class="fa fa-check-circle"></i>
+            </span>
+            <span class="casino-text-menuItemLabel">Todos</span>
+        </div>
+        <div id="featuredMenuItem" class="casino-box-menuItem">
+            <span class="casino-text-menuItemIcon">
+                <i class="fa fa-asterisk"></i>
+            </span>
+            <span class="casino-text-menuItemLabel">Em destaque</span>
+        </div>
+        <div class="casino-box-menuItem">
+            <span class="casino-text-menuItemIcon">
+                <i class="fa fa-credit-card"></i>
+            </span>
+            <span class="casino-text-menuItemLabel">Jogos Cartas</span>
         </div>
     </div>
-    <div class="casino-container-content">
+    <div id="allContainer" class="casino-container-content hidden">
+        <div class="casino-container-header">
+            <div class="acenter">
+                <button class="casino-button">Casino</button>
+                <button class="casino-button">Casino ao vivo</button>
+                <button class="casino-button">Promoções</button>
+            </div>
+        </div>
         <div class="casino-box-header">EM DESTAQUE</div>
         <div class="casino-box">
-            <div class="casino-box-navigation">
-                <button id="featured-prev" class="casino-button-prev"><</button>
-            </div>
+            <button id="featured-prev" class="casino-button-prev"><i class="fa fa-angle-left"></i></button>
             <div class="casino-box-carousel">
                 <div id="featured-carousel" class="owl-carousel">
                     <div class="casino-box-game">
-                        <a href="#"><img class="casino-iframe-game" src="/assets/portal/img/demo/img1.jpg"></a>
+                        <a href="#"><img class="casino-image-game" src="/assets/portal/img/demo/img1.jpg"></a>
                         <span class="casino-text-gameLabel">Wonder Women</span>
                         <button class="fa fa-star casino-button-favorite"></button>
                     </div>
                     <div class="casino-box-game">
-                        <a href="#"><img class="casino-iframe-game" src="/assets/portal/img/demo/img2.jpg"></a>
+                        <a href="#"><img class="casino-image-game" src="/assets/portal/img/demo/img2.jpg"></a>
                         <span class="casino-text-gameLabel">Green Lantern</span>
                         <button class="fa fa-star casino-button-favorite"></button>
                     </div>
                     <div class="casino-box-game">
-                        <a href="#"><img class="casino-iframe-game" src="/assets/portal/img/demo/img3.jpg"></a>
+                        <a href="#"><img class="casino-image-game" src="/assets/portal/img/demo/img3.jpg"></a>
                         <span class="casino-text-gameLabel">The Flash</span>
                         <button class="fa fa-star casino-button-favorite"></button>
                     </div>
                     <div class="casino-box-game">
-                        <a href="#"><img class="casino-iframe-game" src="/assets/portal/img/demo/img4.jpg"></a>
+                        <a href="#"><img class="casino-image-game" src="/assets/portal/img/demo/img4.jpg"></a>
                         <span class="casino-text-gameLabel">Superman</span>
                         <button class="fa fa-star casino-button-favorite"></button>
                     </div>
                     <div class="casino-box-game">
-                        <a href="#"><img class="casino-iframe-game" src="/assets/portal/img/demo/img5.jpg"></a>
+                        <a href="#"><img class="casino-image-game" src="/assets/portal/img/demo/img5.jpg"></a>
                         <span class="casino-text-gameLabel">Champions Goal</span>
                         <button class="fa fa-star casino-button-favorite"></button>
                     </div>
                 </div>
             </div>
-            <div class="casino-box-navigation">
-                <button id="featured-next" class="casino-button-next">></button>
-            </div>
+            <button id="featured-next" class="casino-button-next"><i class="fa fa-angle-right"></i></button>
         </div>
         <div class="casino-box-header">JOGOS DE CARTAS</div>
         <div class="casino-box">
-            <div class="casino-box-navigation">
-                <button id="cards-prev" class="casino-button-prev"><</button>
-            </div>
+            <button id="cards-prev" class="casino-button-prev"><i class="fa fa-angle-left"></i></button>
             <div class="casino-box-carousel">
                 <div id="cards-carousel" class="owl-carousel">
                     <div class="casino-box-game">
-                        <a href="#"><img class="casino-iframe-game" src="/assets/portal/img/demo/img6.jpg"></a>
+                        <a href="#"><img class="casino-image-game" src="/assets/portal/img/demo/img6.jpg"></a>
                         <span class="casino-text-gameLabel">Eletric SAM</span>
                         <button class="fa fa-star casino-button-favorite"></button>
                     </div>
                     <div class="casino-box-game">
-                        <a href="#"><img class="casino-iframe-game" src="/assets/portal/img/demo/img7.jpg"></a>
+                        <a href="#"><img class="casino-image-game" src="/assets/portal/img/demo/img7.jpg"></a>
                         <span class="casino-text-gameLabel">The Lab</span>
                         <button class="fa fa-star casino-button-favorite"></button>
                     </div>
                     <div class="casino-box-game">
-                        <a href="#"><img class="casino-iframe-game" src="/assets/portal/img/demo/img8.jpg"></a>
+                        <a href="#"><img class="casino-image-game" src="/assets/portal/img/demo/img8.jpg"></a>
                         <span class="casino-text-gameLabel">DJ Wild</span>
                         <button class="fa fa-star casino-button-favorite"></button>
                     </div>
                     <div class="casino-box-game">
-                        <a href="#"><img class="casino-iframe-game" src="/assets/portal/img/demo/img9.jpg"></a>
+                        <a href="#"><img class="casino-image-game" src="/assets/portal/img/demo/img9.jpg"></a>
                         <span class="casino-text-gameLabel">Wolf PACK</span>
                         <button class="fa fa-star casino-button-favorite"></button>
                     </div>
                     <div class="casino-box-game">
-                        <a href="#"><img class="casino-iframe-game" src="/assets/portal/img/demo/img10.jpg"></a>
+                        <a href="#"><img class="casino-image-game" src="/assets/portal/img/demo/img10.jpg"></a>
                         <span class="casino-text-gameLabel">Wild RIDE</span>
                         <button class="fa fa-star casino-button-favorite"></button>
                     </div>
                 </div>
             </div>
-            <div class="casino-box-navigation">
-                <button id="cards-next" class="casino-button-next">></button>
+            <button id="cards-next" class="casino-button-next"><i class="fa fa-angle-right"></i></button>
+        </div>
+    </div>
+    <div id="featuredGamesContainer" class="casino-container-content">
+        <div class="casino-container-header">
+            <div class="acenter">
+                <button class="casino-button">Casino</button>
+                <button class="casino-button">Casino ao vivo</button>
+                <button class="casino-button">Promoções</button>
             </div>
         </div>
+        <div class="casino-box-header">EM DESTAQUE</div>
+            <div class="casino-box-game">
+                <a href="#"><img class="casino-image-game" src="/assets/portal/img/demo/img1.jpg"></a>
+                <span class="casino-text-gameLabel">Wonder Women</span>
+                <button class="fa fa-star casino-button-favorite"></button>
+            </div>
+            <div class="casino-box-game">
+                <a href="#"><img class="casino-image-game" src="/assets/portal/img/demo/img2.jpg"></a>
+                <span class="casino-text-gameLabel">Green Lantern</span>
+                <button class="fa fa-star casino-button-favorite"></button>
+            </div>
+            <div class="casino-box-game">
+                <a href="#"><img class="casino-image-game" src="/assets/portal/img/demo/img3.jpg"></a>
+                <span class="casino-text-gameLabel">The Flash</span>
+                <button class="fa fa-star casino-button-favorite"></button>
+            </div>
+            <div class="casino-box-game">
+                <a href="#"><img class="casino-image-game" src="/assets/portal/img/demo/img4.jpg"></a>
+                <span class="casino-text-gameLabel">Superman</span>
+                <button class="fa fa-star casino-button-favorite"></button>
+            </div>
+            <div class="casino-box-game">
+                <a href="#"><img class="casino-image-game" src="/assets/portal/img/demo/img5.jpg"></a>
+                <span class="casino-text-gameLabel">Champions Goal</span>
+                <button class="fa fa-star casino-button-favorite"></button>
+            </div>
+        </div>
+        <div style="clear: both"></div>
     </div>
 </div>
 @stop
