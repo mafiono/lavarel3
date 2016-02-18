7fa9eb5d5e623e876d3c4aab944f4853756b0ad5
José Miguel Couto <jmiguelcouto@gmail.com>
2016-02-18 16:29:58 +0000

    Changed Scripts

--- database/dump/backupDbs.bat
+++ database/dump/backupDbs.bat
@@ -4,4 +4,12 @@ cd ..
 mysqldump -u root -pibetup -d -B ibetupze --skip-comments | sed 's/ AUTO_INCREMENT=[0-9]*\b//' > "database/dump/ibetupze.sql"
 mysqldump -u root -pibetup -d -B ibetupze2 --skip-comments | sed 's/ AUTO_INCREMENT=[0-9]*\b//' > "database/dump/ibetupze2.sql"
 
+mysqldump -h 160.153.16.53 -u ibetupze -pa6HTaTq.[0uI -d -B ibetupze --skip-comments | sed 's/ AUTO_INCREMENT=[0-9]*\b//' > database/dump/ibetupze_remote.sql
+mysqldump -h 160.153.16.53 -u ibetupze -pa6HTaTq.[0uI -d -B ibetupco --skip-comments | sed 's/ AUTO_INCREMENT=[0-9]*\b//' > database/dump/ibetupco_remote.sql
+
+mysqldump -u root -pibetup ibetupze > "database/dump/full_ibetupze.sql"
+mysqldump -u root -pibetup ibetupco > "database/dump/full_ibetupco.sql"
+
+
+mysqldump -h 160.153.16.53 -u ibetupze -pa6HTaTq.[0uI ibetupco > database/dump/full_ibetupco_remote.sql
 pause
