a86971ec8dca7b75753d9c29776da07fa83733b3
José Miguel Couto <jmiguelcouto@gmail.com>
2016-03-01 14:20:01 +0000

    Changed Faqs Design.

---
+++ resources/views/portal/info/footer.blade.php
@@ -0,0 +1,11 @@
+<div class="col-xs-12 footer neut-back neut-color" style="clear: both;">
+    <ul>
+        <li><a href="/sobre_nos" onclick="onPopup(this); return false;" class="btn btn-grey">Politica de Privacidade</a></li>
+        <li><a href="/afiliados" onclick="onPopup(this); return false;" class="btn btn-grey">Politica de Cookies</a></li>
+        <li><a href="/termos_e_condicoes" onclick="onPopup(this); return false;" class="btn btn-grey">Regras</a></li>
+        <li><a href="/contactos" onclick="onPopup(this); return false;" class="btn btn-grey">Dificuldades Tecnicas</a></li>
+        <li><a href="/ajuda" onclick="onPopup(this); return false;" class="btn btn-grey">Jogo Responsavel</a></li>
+        <div class="clear"></div>
+    </ul>
+    <div class="clear"></div>
+</div>
