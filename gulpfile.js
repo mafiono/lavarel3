4bbd124b6a2bd300dd039857f758fc97572ea841
José Miguel Couto <jmiguelcouto@gmail.com>
2016-03-07 17:24:32 +0000

    Added NPM and gulp watch with browser-sync.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,37 @@
-var elixir = require('laravel-elixir');
+var gulp        = require('gulp'),
+    php         = require('gulp-connect-php'),
+    browserSync = require('browser-sync').create(),
+    less        = require('gulp-less');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+var config = {
+    source: './resources/assets/',
+    less: 'less/*.less',
+    dest: './public/assets/',
+    styles: 'portal/newstyle',
+    blades: 'resources/views/*.blade.php'
+};
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+// Static Server + watching less/html files
+gulp.task('browser-sync', ['artisan', 'less'], function() {
+    browserSync.init({
+        proxy: 'http://127.0.0.1:8000'
+    });
+    gulp.watch(config.source + config.less, ['less']);
+    gulp.watch(config.blades).on('change', browserSync.reload);
 });
+
+// Compile less into CSS & auto-inject into browsers
+gulp.task('less', function() {
+    return gulp.src(config.source + config.less)
+        .pipe(less())
+        .pipe(gulp.dest(config.dest + config.styles))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('artisan', function () {
+    php.server({
+        base: './public'
+    });
+});
+
+gulp.task('watch', ['browser-sync'])
