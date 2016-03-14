23cc3dee6ee33599edaad65fd586c6c860651462
José Miguel Couto <jmiguelcouto@gmail.com>
2016-03-14 10:40:14 +0000

    Fix Registo Part 1.

--- resources/views/portal/sign_up/footer.blade.php
+++ resources/views/portal/sign_up/footer.blade.php
@@ -11,19 +11,17 @@
         @endfor
     </div>
     <div class="col-xs-4 form-submit fleft">
-        <div class="row">
-            <div class="col-xs-offset-2 col-xs-9 btns aright">
-                <input type="submit" class="col-xs-5 brand-botao brand-link formSubmit fright" value="Concluir" />
-                @if (!empty($skip))
-                    <a href="{{$skip}}" class="col-xs-6 btn btn-brand fleft">Saltar Passo</a>
-                @endif
-                @if (!empty($back))
-                    <a href="{{$back}}" class="col-xs-6 btn btn-brand fleft">Voltar</a>
-                @endif
-                @if (!empty($play))
-                    <a href="{{$play}}" class="col-xs-6 btn btn-brand fleft">Jogar</a>
-                @endif
-            </div>
+        <div class="btns aright">
+            <input type="submit" class="col-xs-5 brand-botao brand-link formSubmit fright" value="Concluir" />
+            @if (!empty($skip))
+                <a href="{{$skip}}" class="col-xs-6 btn btn-brand fleft">Saltar Passo</a>
+            @endif
+            @if (!empty($back))
+                <a href="{{$back}}" class="col-xs-6 btn btn-brand fleft">Voltar</a>
+            @endif
+            @if (!empty($play))
+                <a href="{{$play}}" class="col-xs-6 btn btn-brand fleft">Jogar</a>
+            @endif
         </div>
     </div>
     <div class="clear"></div>
--- resources/views/portal/sign_up/step_1.blade.php
+++ resources/views/portal/sign_up/step_1.blade.php
@@ -20,81 +20,81 @@
                         <div class="col-xs-10 grid">
                             <div class="row">
                                 <div class="col-xs-4">
-                                <div class="title-form-registo brand-title brand-color aleft">
-                                    Informação Pessoal
-                                </div>
+                                    <div class="title-form-registo brand-title brand-color aleft">
+                                        Informação Pessoal
+                                    </div>
 
-                                <div class="registo-form">
-                                    <label>Título</label>
-                                    <select name="gender" id="gender">
-                                        @if (!empty($inputs) && $inputs['gender'] == 'f')
-                                            <option value="m">Sr.</option>
-                                            <option value="f" selected>Sra.</option>
-                                        @else
-                                            <option value="m" selected>Sr.</option>
-                                            <option value="f">Sra.</option>
-                                        @endif
-                                    </select>
-                                    <span class="has-error error" style="display:none;"> </span>
-                                </div>
-
-                                <div class="registo-form">
-                                    <label>Nome Completo</label>
-                                    <input type="text" name="name" id="name" class="required" value="<?php echo !empty($inputs) ? $inputs['name'] : ''?>"/>
-                                    <span class="has-error error" style="display:none;"> </span>
-                                </div>
-
-                                <div class="registo-form">
-                                    <label>Data de Nascimento</label>
-                                    <div class="">
-                                        <select name="age_day">
-                                            <?php for ($i=1; $i < 32; $i++): ?>
-                                                <option value='{{$i}}' <?php echo !empty($inputs) && $inputs['age_day'] == $i ? 'selected'  : ''?>><?php echo sprintf("%02d",$i)?></option>
-                                            <?php endfor?>
+                                    <div class="registo-form">
+                                        <label>Título</label>
+                                        <select name="gender" id="gender">
+                                            @if (!empty($inputs) && $inputs['gender'] == 'f')
+                                                <option value="m">Sr.</option>
+                                                <option value="f" selected>Sra.</option>
+                                            @else
+                                                <option value="m" selected>Sr.</option>
+                                                <option value="f">Sra.</option>
+                                            @endif
                                         </select>
-                                        <select name="age_month">
-                                            <?php for ($i=1; $i < 13; $i++): ?>
-                                                <option value='{{$i}}' <?php echo !empty($inputs) && $inputs['age_month'] == $i ? 'selected'  : ''?>><?php echo sprintf("%02d",$i)?></option>
-                                            <?php endfor?>
-                                        </select>
-                                        <select name="age_year">
-                                            <?php
-                                            $year = \Carbon\Carbon::now()->subYears(18)->year;
-                                            // $year = date("Y") - 18;
-                                            for ($i=$year; $i > $year - 95; $i--): ?>
-                                                <option value='{{$i}}' <?php echo !empty($inputs) && $inputs['age_year'] == $i ? 'selected'  : ''?>>{{$i}}</option>
-                                            <?php endfor?>
-                                        </select>
-                                        <input name="birth_date" type="hidden">
                                         <span class="has-error error" style="display:none;"> </span>
                                     </div>
-                                    <div class="clear"></div>
-                                </div>
 
-                                <div class="registo-form">
-                                    <label>Nacionalidade</label>
-                                    {!! Form::select('nationality', $natList, !empty($inputs) ? $inputs['nationality'] : 'Português') !!}
-                                    <span class="has-error error" style="display:none;"> </span>
-                                </div>
+                                    <div class="registo-form">
+                                        <label>Nome Completo</label>
+                                        <input type="text" name="name" id="name" class="required" value="<?php echo !empty($inputs) ? $inputs['name'] : ''?>"/>
+                                        <span class="has-error error" style="display:none;"> </span>
+                                    </div>
 
-                                <div class="registo-form">
-                                    <label>Nº Identificação civil</label>
-                                    <input type="text" name="document_number" id="document_number" class="required" value="<?php echo !empty($inputs) ? $inputs['document_number'] : ''?>"/>
-                                    <span class="has-error error" style="display:none;"> </span>
-                                    <p style="display: none">Bi, Cartão Cidadão, Passaporte, Carta de Condução</p>
-                                </div>
+                                    <div class="registo-form">
+                                        <label>Data de Nascimento</label>
+                                        <div class="">
+                                            <select name="age_day">
+                                                <?php for ($i=1; $i < 32; $i++): ?>
+                                                    <option value='{{$i}}' <?php echo !empty($inputs) && $inputs['age_day'] == $i ? 'selected'  : ''?>><?php echo sprintf("%02d",$i)?></option>
+                                                <?php endfor?>
+                                            </select>
+                                            <select name="age_month">
+                                                <?php for ($i=1; $i < 13; $i++): ?>
+                                                    <option value='{{$i}}' <?php echo !empty($inputs) && $inputs['age_month'] == $i ? 'selected'  : ''?>><?php echo sprintf("%02d",$i)?></option>
+                                                <?php endfor?>
+                                            </select>
+                                            <select name="age_year">
+                                                <?php
+                                                $year = \Carbon\Carbon::now()->subYears(18)->year;
+                                                // $year = date("Y") - 18;
+                                                for ($i=$year; $i > $year - 95; $i--): ?>
+                                                    <option value='{{$i}}' <?php echo !empty($inputs) && $inputs['age_year'] == $i ? 'selected'  : ''?>>{{$i}}</option>
+                                                <?php endfor?>
+                                            </select>
+                                            <input name="birth_date" type="hidden">
+                                            <span class="has-error error" style="display:none;"> </span>
+                                        </div>
+                                        <div class="clear"></div>
+                                    </div>
 
-                                <div class="registo-form">
-                                    <label>NIF</label>
-                                    <input type="text" name="tax_number" id="tax_number" class="required" value="<?php echo !empty($inputs) ? $inputs['tax_number'] : ''?>"/>
-                                    <span class="has-error error" style="display:none;"> </span>
-                                </div>
+                                    <div class="registo-form">
+                                        <label>Nacionalidade</label>
+                                        {!! Form::select('nationality', $natList, !empty($inputs) ? $inputs['nationality'] : 'Português') !!}
+                                        <span class="has-error error" style="display:none;"> </span>
+                                    </div>
 
-                                <div class="registo-form">
-                                    <label>Profissão</label>
-                                    <input type="text" name="profession" id="profession" class="required" value="<?php echo !empty($inputs) ? $inputs['profession'] : ''?>"/>
-                                    <span class="has-error error" style="display:none;"> </span>
-                                </div>
+                                    <div class="registo-form">
+                                        <label>Nº Identificação civil</label>
+                                        <input type="text" name="document_number" id="document_number" class="required" value="<?php echo !empty($inputs) ? $inputs['document_number'] : ''?>"/>
+                                        <span class="has-error error" style="display:none;"> </span>
+                                        <p style="display: none">Bi, Cartão Cidadão, Passaporte, Carta de Condução</p>
+                                    </div>
+
+                                    <div class="registo-form">
+                                        <label>NIF</label>
+                                        <input type="text" name="tax_number" id="tax_number" class="required" value="<?php echo !empty($inputs) ? $inputs['tax_number'] : ''?>"/>
+                                        <span class="has-error error" style="display:none;"> </span>
+                                    </div>
+
+                                    <div class="registo-form">
+                                        <label>Profissão</label>
+                                        <input type="text" name="profession" id="profession" class="required" value="<?php echo !empty($inputs) ? $inputs['profession'] : ''?>"/>
+                                        <span class="has-error error" style="display:none;"> </span>
+                                    </div>
                                 </div>
                                 <div class="col-xs-4 middle">
                                     <div class="title-form-registo brand-title brand-color aleft">
@@ -196,8 +196,12 @@
                             </div>
                         </div>
                     </div>
+                    <div class="row">
+                        <div class="col-xs-offset-2 col-xs-10 grid">
+                            @include('portal.sign_up.footer', ['step' => 1])
+                        </div>
+                    </div>
                     <div class="clear"></div>
-                    @include('portal.sign_up.footer', ['step' => 1])
                     {!! Form::close() !!}
                 </div>
             </div>
