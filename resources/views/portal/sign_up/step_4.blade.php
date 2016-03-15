7cdf853040dd07cbbbd5a5a6e2ab337659347878
José Miguel Couto <jmiguelcouto@gmail.com>
2016-03-15 12:30:06 +0000

    Fix Regist Step 2, 3, 4.

--- resources/views/portal/sign_up/step_4.blade.php
+++ resources/views/portal/sign_up/step_4.blade.php
@@ -8,34 +8,40 @@
                 @include('portal.partials.pop_header_signup', ['text' => empty($selfExclusion) && empty($identity) ?
                  'Esta pronto para Jogar!': ''])
 
-                <div class="form-registo">
-                    <div class="col-xs-2 lin-xs-12 fleft">
-                        <div class="lin-xs-12 banner-back box-form-registo">
-                            <img src="/assets/portal/img/banners/banner_ronaldo.png" alt="Banner Registo">
-                        </div>
-                    </div>
-                    <div class="col-xs-10 lin-xs-9 fleft">
-                        <div class="col-xs-12 fleft">
-                            <div class="box-form-registo">
-                                @if (empty($selfExclusion) && empty($identity))
-                                    <div class="title-form-confirma brand-title brand-color acenter">
-                                        A sua conta foi criada com sucesso!
-                                    </div>
-
-                                    <div class="col-xs-12 brand-descricao media-mbottom acenter">
-                                        Foi enviado um email para a sua conta de correio, por favor valide esse email para poder começar a jogar!
-                                    </div>
-
-                                    @include('portal.bank.deposit_partial')
-
-                                    <div class="media-mbottom"></div>
-
-                                @endif
+                <div class="form-registo bs-wp">
+                    <div class="row">
+                        <div class="col-xs-2">
+                            <div class="banner-back">
+                                <img src="/assets/portal/img/banners/banner_postiga.png" alt="Banner Registo">
                             </div>
                         </div>
-                        <div class="clear"></div>
+                        <div class="col-xs-10 grid">
+                            <div class="row">
+                                <div class="box-form-registo">
+                                    @if (empty($selfExclusion) && empty($identity))
+                                        <div class="title-form-confirma brand-title brand-color acenter">
+                                            A sua conta foi criada com sucesso!
+                                        </div>
 
-                        @include('portal.sign_up.footer', ['step' => 4, 'play' => '/'])
+                                        <div class="col-xs-12 brand-descricao media-mbottom acenter">
+                                            Foi enviado um email para a sua conta de correio, por favor valide esse email para poder começar a jogar!
+                                        </div>
+
+                                        <div class="col-xs-offset-2 col-xs-9">
+                                            @include('portal.bank.deposit_partial')
+                                        </div>
+
+                                        <div class="media-mbottom"></div>
+
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-offset-2 col-xs-10 grid">
+                            @include('portal.sign_up.footer', ['step' => 4, 'play' => '/'])
+                        </div>
                     </div>
                     <div class="clear"></div>
                 </div>
