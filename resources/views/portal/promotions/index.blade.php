88f40fda6bea1e53b6be4339dd8242ca1caded9a
José Miguel Couto <jmiguelcouto@gmail.com>
2016-04-01 18:34:56 +0100

    Fix Promotions Layout tables.

--- resources/views/portal/promotions/index.blade.php
+++ resources/views/portal/promotions/index.blade.php
@@ -21,41 +21,37 @@
             </div><!-- /.modal-content -->
         </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
-    <div class="col-xs-12 lin-xs-11 fleft">
-        <div class="box-form-user-info lin-xs-12">
-            <div class="title-form-registo brand-title brand-color aleft">
-                Promoções e Bónus para utilização
-            </div>
-            @include('portal.messages')
-            <div class="table_user mini-mbottom">
-                <table class="col-xs-12">
-                    <thead>
-                        <tr>
-                            <th class="col-xs-2 acenter">Origem</th>
-                            <th class="col-xs-4 acenter">Tipo</th>
-                            <th class="col-xs-2 acenter">Bónus</th>
-                            <th class="col-xs-2 acenter">Código</th>
-                            <th class="col-xs-2 acenter">Opções</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        @foreach($availableBonuses as $bonus)
-                        <tr>
-                            <td class="col-xs-2 acenter">Desporto</td>
-                            <td class="col-xs-4 acenter">{{$bonus->bonus_type}} <li class="fa fa-info-circle brand-color"></li></td>
-                            <td class="col-xs-2 acenter">{{$bonus->value}}</td>
-                            <td class="col-xs-2 acenter success-color"><b>{{$bonus->title}}</b></td>
-                            <td class="col-xs-2 acenter neut-back">
-                                {{--<button class="brand-botao brand-botao-mini brand-link" data-toggle="modal" data-target="#redeem-modal">Resgatar</button>--}}
-                                <a href="/promocoes/redeem/{{$bonus->id}}" class="brand-botao brand-botao-mini brand-link" onclick="return confirmRedeem('{{$bonus->title}}')">Resgatar</a>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+    <div class="col-xs-12 fleft">
+        <div class="title-form-registo brand-title brand-color aleft">
+            Promoções e Bónus para utilização
         </div>
+        @include('portal.messages')
+        <table class="settings-table">
+            <thead>
+                <tr>
+                    <th class="col-xs-2 acenter">Origem</th>
+                    <th class="col-xs-4 acenter">Tipo</th>
+                    <th class="col-xs-2 acenter">Bónus</th>
+                    <th class="col-xs-2 acenter">Código</th>
+                    <th class="col-xs-2 acenter">Opções</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach($availableBonuses as $bonus)
+                <tr>
+                    <td class="col-xs-2 acenter">Desporto</td>
+                    <td class="col-xs-4 acenter">{{$bonus->bonus_type}} <li class="fa fa-info-circle brand-color"></li></td>
+                    <td class="col-xs-2 acenter">{{$bonus->value}}</td>
+                    <td class="col-xs-2 acenter success-color"><b>{{$bonus->title}}</b></td>
+                    <td class="col-xs-2 acenter neut-back">
+                        {{--<button class="brand-botao brand-botao-mini brand-link" data-toggle="modal" data-target="#redeem-modal">Resgatar</button>--}}
+                        <a href="/promocoes/redeem/{{$bonus->id}}" class="brand-botao brand-botao-mini brand-link" onclick="return confirmRedeem('{{$bonus->title}}')">Resgatar</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
     <div class="clear"></div>
 @stop
