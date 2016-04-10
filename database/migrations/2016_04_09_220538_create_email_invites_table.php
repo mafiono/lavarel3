851b9bf28534565e55495ba194422ecba6c27c69
Miguel Teixeira <miguel.teixeira@programmer.net>
2016-04-10 16:27:24 +0100

    Added: Interval date for bulk email invites to avoid spam. Fixed: Usersetting.php bad merge.

--- database/migrations/2016_04_09_220538_create_email_invites_table.php
+++ database/migrations/2016_04_09_220538_create_email_invites_table.php
@@ -16,6 +16,7 @@ class CreateEmailInvitesTable extends Migration
             $table->integer('user_id')->unsigned();
             $table->string('email', 45);
             $table->timestamps();
+            $table->primary(['user_id', 'email']);
             $table->foreign('user_id')->references('id')->on('users');
         });
     }
