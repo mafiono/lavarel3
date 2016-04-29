708a511282874409431bf6155e8e4ec1acdce27a
Miguel Teixeira <miguel.teixeira@programmer.net>
2016-04-29 12:10:01 +0100

    modified: staff migration.

--- database/migrations/2016_04_28_154134_update_staff_table.php
+++ database/migrations/2016_04_28_154134_update_staff_table.php
@@ -19,20 +19,20 @@ class UpdateStaffTable extends Migration
             $table->string('mobile_personal', 15);
             $table->string('marital_status', 10);
             $table->string('citizen_card', 15);
-            $table->boolean('health_insurance');
-            $table->boolean('owns_car');
+            $table->string('health_insurance');
+            $table->string('owns_car');
             $table->unsignedTinyInteger('written_english');
             $table->unsignedTinyInteger('oral_english');
             $table->unsignedTinyInteger('computer_skills');
             $table->text('personal_interests');
             $table->text('vacations_activities');
             $table->text('software_experience');
-            $table->string('pc_declaration_file', 45);
-            $table->string('citizen_card_file', 45);
-            $table->string('curriculum_vitae_file', 45);
-            $table->string('qualifications_certificate_file', 45);
-            $table->string('nda_file', 45);
-            $table->string('work_contract_file', 45);
+            $table->string('upload_pc_declaration', 45);
+            $table->string('upload_citizen_card', 45);
+            $table->string('upload_curriculum_vitae', 45);
+            $table->string('upload_qualifications_certificate', 45);
+            $table->string('upload_nda', 45);
+            $table->string('upload_work_contract', 45);
         });
     }
 
@@ -58,12 +58,12 @@ class UpdateStaffTable extends Migration
             $table->dropColumn('personal_interests');
             $table->dropColumn('vacations_activities');
             $table->dropColumn('software_experience');
-            $table->dropColumn('pc_declaration_file');
-            $table->dropColumn('citizen_card_file');
-            $table->dropColumn('curriculum_vitae_file');
-            $table->dropColumn('qualifications_certificate_file');
-            $table->dropColumn('nda_file');
-            $table->dropColumn('work_contract_file');
+            $table->dropColumn('upload_pc_declaration');
+            $table->dropColumn('upload_citizen_card');
+            $table->dropColumn('upload_curriculum_vitae');
+            $table->dropColumn('upload_qualifications_certificate');
+            $table->dropColumn('upload_nda');
+            $table->dropColumn('upload_work_contract');
         });
     }
 }
