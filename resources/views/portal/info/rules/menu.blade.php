31f7359b9f084accf24714cdaae9672e78dd48a5
Miguel Couto <jmiguelcouto@gmail.com>
2016-05-25 18:23:55 +0100

    Removed Casino from Rules. Added a Email link to Contacts.

--- resources/views/portal/info/rules/menu.blade.php
+++ resources/views/portal/info/rules/menu.blade.php
@@ -1,6 +1,6 @@
 <div class="top">
     <ul class="menu">
-        <li><a {{$tipo=='casino'?'class=sel':''}} href="/info/regras/casino">Casino</a></li>
+        {{--<li><a {{$tipo=='casino'?'class=sel':''}} href="/info/regras/casino">Casino</a></li>--}}
         <li><a {{$tipo=='sports'?'class=sel':''}} href="/info/regras/sports">Apostas Desportivas</a></li>
     </ul>
 </div>
