398299cc73873da92e16fc03e0b8019e4fcc3b16
Miguel Couto <jmiguelcouto@gmail.com>
2016-05-26 13:15:49 +0100

    Added comment.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -13,7 +13,7 @@
             <img alt="Aviso Maioridade" src="/assets/portal/img/18plus.png" />
         </div>
         <div class="footer-aviso">
-            A BetPortugal encontra-se licenciada e regulada pelo Governo de Portugal e opera ao abrigo da Licença de Jogo Nº ###/## efectuando exploração de jogos de fortuna e azar on-line onde o operador está sujeito a monitorização e inspecção do SRIJ.
+            A BetPortugal encontra-se licenciada e regulada pelo Governo de Portugal e opera ao abrigo da Licença de Jogo Nº ###/## efectuando exploração de apostas e jogos de fortuna e azar on-line onde o operador está sujeito a monitorização e inspecção do SRIJ.
             <br />
             <br />
             Ao aceder, continuar a utilizar ou a navegar nesta página web, aceita que utilizemos certos cookies de navegador com o objectivo de melhorar a sua experiência. A BetPortugal apenas usa cookies que melhoram a sua experiência e não interferem com a sua privacidade. Queira por favor aceder à nossa Política de Utilização para mais informação relativamente à forma como utilizamos cookies e como pode desactivar ou gerir os mesmos, caso deseje fazê-lo.
