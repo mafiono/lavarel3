883334e565b9ae030be599732830eb1cb1b1385b
Miguel Couto <jmiguelcouto@gmail.com>
2016-05-31 10:20:11 +0100

    Added Info Controller to support Doc pages on Mobile.

---
+++ app/Http/Controllers/Api/InfoController.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\LegalDoc;
+use Response;
+use Illuminate\Http\Request;
+
+class InfoController extends Controller
+{
+    protected $authUser;
+    protected $request;
+    protected $userSessionId;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function getDocInfo(Request $request) {
+        $docId = $request->get('doc');
+        $legalDoc = LegalDoc::getDoc($docId);
+        return Response::json(compact('legalDoc'));
+    }
+}
