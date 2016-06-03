ed3c0956b2ae77dae383352cfeea7d7a29e7ce78
Miguel Couto <jmiguelcouto@gmail.com>
2016-06-03 17:21:49 +0100

    Fix dates not equals.

--- app/User.php
+++ app/User.php
@@ -219,7 +219,26 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
         'iban.digits' => 'O Iban é composto por 23 caracteres, excluíndo os primeiros dois dígitos PT',
     );
 
-  /**
+    /**
+     * Compare if 2 dates are not equal
+     * @param Carbon $a
+     * @param Carbon $b
+     * @return bool
+     */
+    private static function datesNotEquals(Carbon $a, Carbon $b)
+    {
+        if ($a === null && $b === null)
+            return false;
+        if ($a !== null && $b === null)
+            return true;
+        if ($a === null && $b !== null)
+            return true;
+        if ($a->diffInHours($b) > 1)
+            return true;
+        return false;
+    }
+
+    /**
     * Relation with User Profile
     *
     */
@@ -1308,11 +1327,8 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
                 // Add to self exclusion
                 if ($selfExclusion != null){
                     // Check if its the same
-                    if ($selfExclusion->request_date->diffInHours($selfExclusionSRIJ->start_date) > 1
-                        || ($selfExclusion->end_date != $selfExclusionSRIJ->end_date) // TODO rethink this logic
-                        || ($selfExclusion->end_date == null && $selfExclusionSRIJ->end_date != null)
-                        || ($selfExclusion->end_date != null && $selfExclusionSRIJ->end_date == null)
-                        || $selfExclusion->end_date->diffInHours($selfExclusionSRIJ->end_date) > 1){
+                    if (self::datesNotEquals($selfExclusion->request_date, $selfExclusionSRIJ->start_date)
+                        || self::datesNotEquals($selfExclusion->end_date, $selfExclusionSRIJ->end_date)){
                         // Update if its not
                         if (! $userSession = $this->logUserSession('self_exclusion.from_srij', 'self-exclusion from SRIJ'))
                             throw new Exception('Error creating Session!');
