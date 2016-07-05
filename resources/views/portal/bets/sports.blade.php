ce6b1895fdbdf1075df8b4244465c06192580667
Miguel Teixeira <miguel.teixeira@programmer.net>
2016-07-05 16:54:06 +0100

    List favorites

--- resources/views/portal/bets/sports.blade.php
+++ resources/views/portal/bets/sports.blade.php
@@ -52,5 +52,6 @@
     <script src="assets/portal/js/sports/sportsMenu.js" defer></script>
     <script src="assets/portal/js/sports/Markets.js" defer></script>
     <script src="assets/portal/js/sports/Betslip.js" defer></script>
+    <script src="assets/portal/js/sports/favorites.js" defer></script>
 
 @stop
