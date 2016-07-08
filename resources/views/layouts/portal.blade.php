2a67201c3e771fcfee8e86515bb0e88718aeb275
Diogo Araujo <diogoboltaraujo@gmail.com>
2016-07-08 15:58:23 +0100

    final fix tawk.to

--- resources/views/layouts/portal.blade.php
+++ resources/views/layouts/portal.blade.php
@@ -100,7 +100,7 @@
         (function(){
             var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
             s1.async=true;
-            s1.src='https://embed.tawk.to/576aa05d888093210eb097bc/default';
+            s1.src='https://embed.tawk.to/576a9987b57c05002099a2e3/default';
             s1.charset='UTF-8';
             s1.setAttribute('crossorigin','*');
             s0.parentNode.insertBefore(s1,s0);
