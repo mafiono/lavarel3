79c9e4f6b1358753343364e34e68a1d04cf4742f
Miguel Teixeira <miguel.teixeira@programmer.net>
2016-08-10 11:03:05 +0100

    Alterar @ para © #36

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -28,7 +28,7 @@
         <p>A BetPortugal encontra-se licenciada e regulada pelo Governo de Portugal e opera ao abrigo da Licença de Jogo Nº ###/## efectuando exploração de apostas e jogos de fortuna e azar on-line onde o operador está sujeito a monitorização e inspecção do SRIJ.</p>
         <p>Ao aceder, continuar a utilizar ou a navegar nesta página web, aceita que utilizemos certos cookies de navegador com o objectivo de melhorar a sua experiência. A BetPortugal apenas usa cookies que melhoram a sua experiência e não interferem com a sua privacidade. Queira por favor aceder à nossa Política de Utilização para mais informação relativamente à forma como utilizamos cookies e como pode desactivar ou gerir os mesmos, caso deseje fazê-lo.</p>
 
-        <p class="copyright">SFP Online S.A. @ 2016. Todos os direitos reservados.</p>
+        <p class="copyright">SFP Online S.A. © 2016. Todos os direitos reservados.</p>
         <div class="payments"><img src="/assets/portal/img/footer/payments.jpg" alt="pagamentos"></div>
     </div>
 </div>
