d31909d1a575dbd0c3815510c181cf9197cdf138
miguel.teixeira <miguel.teixeira@sfponline.pt>
2016-09-05 11:16:58 +0100

    Pequenas correções.

--- resources/assets/js/updaters/fixturesMenuUpdater.js
+++ resources/assets/js/updaters/fixturesMenuUpdater.js
@@ -30,7 +30,7 @@ FixturesMenuUpdater = new (function() {
 
             var matchState = $(".sportsMenu div[data-game-id=" + fixture.id + "] .matchState");
 
-            matchState.html(Template.apply("match_state", fixture));
+            matchState.html(fixture.elapsed + "'<br>" + fixture.score);
         }
     }
 
