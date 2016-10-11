7f4b25f7b5741650336001d03ab69c79c03f1625
Miguel Teixeira <miguel.teixeira@sfponline.pt>
2016-10-11 17:59:11 +0100

    Adicionar limites ao nº de apostas do betslip e ao prémio máximo ref #86

--- resources/assets/js/sports/betslip.js
+++ resources/assets/js/sports/betslip.js
@@ -247,8 +247,12 @@ Betslip = new (function () {
         return elem.data("old-amount");
     }
 
-    function canAdd(bet)
-    {
+    function canAdd(bet) {
+        if (bets.length > 19) {
+            alert("Não pode ultrapassar 20 apostas.");
+            return false;
+        }
+
         if (betMode == "simple")
             return true;
 
