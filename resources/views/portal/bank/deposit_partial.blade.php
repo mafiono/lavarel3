cb0d6cbd3856228ea2e5bb9c0f966a155601fe72
José Miguel Couto <jose.couto@sfponline.pt>
2016-11-21 12:12:51 +0000

    ref #94: Profile Bank deposit page.

--- resources/views/portal/bank/deposit_partial.blade.php
+++ resources/views/portal/bank/deposit_partial.blade.php
@@ -1,47 +1,62 @@
-{!! Form::open(array('route' => 'banco/depositar', 'class' => 'form', 'id' => 'saveForm')) !!}
+{!! Form::open(array('route' => 'banco/depositar', 'class' => 'form deposit', 'id' => 'saveForm')) !!}
 
-<div class="row icons">
-    <div class="col-xs-6">
-        <input type="radio" name="payment_method" id="method_cc" value="cc" checked="checked">
-        <label for="method_cc">
-            <img src="/assets/portal/img/thumbs/visa.jpg" alt="" border="0"> Visa
-        </label>
+    <div class="row icons">
+        <div class="col-xs-6">
+            <input type="radio" name="payment_method" id="method_cc" value="cc" checked="checked">
+            <label for="method_cc">
+                <img src="/assets/portal/img/thumbs/visa.jpg" alt="" border="0"> Visa
+            </label>
+        </div>
+        <div class="col-xs-6">
+            <input type="radio" name="payment_method" id="method_paypal" value="paypal">
+            <label for="method_paypal">
+                <img src="/assets/portal/img/thumbs/paypal.jpg" alt="" border="0"> Paypal
+            </label>
+        </div>
+        <div class="col-xs-6">
+            <input type="radio" name="payment_method" id="method_mc" value="cc">
+            <label for="method_mc">
+                <img src="/assets/portal/img/thumbs/mastercard.jpg" alt="" border="0"> MasterCard
+            </label>
+        </div>
+        <div class="col-xs-6">
+            <input type="radio" name="payment_method" id="method_mb" value="mb">
+            <label for="method_mb">
+                <img src="/assets/portal/img/thumbs/mb.jpg" alt="" border="0"> Multibanco
+            </label>
+        </div>
+        <div class="col-xs-6">
+            <input type="radio" name="payment_method" id="method_meo_wallet" value="meo_wallet">
+            <label for="method_meo_wallet">
+                <img src="/assets/portal/img/thumbs/wallet.jpg" alt="" border="0"> Meo Wallet
+            </label>
+        </div>
+        <div class="col-xs-6">
+            <input type="radio" name="payment_method" id="method_tb" value="tb">
+            <label for="method_tb">
+                <img src="/assets/portal/img/thumbs/trans_bank.jpg" alt="" border="0"> Transf. Bancária
+            </label>
+        </div>
     </div>
-    <div class="col-xs-6">
-        <input type="radio" name="payment_method" id="method_paypal" value="paypal">
-        <label for="method_paypal">
-            <img src="/assets/portal/img/thumbs/paypal.jpg" alt="" border="0"> Paypal
-        </label>
+    <div class="form-group row amount">
+        <div class="col-xs-7">
+            {!! Form::label('value', 'montante que pretende depositar') !!}
+        </div>
+        <div class="col-xs-5">
+            <div class="input-group">
+                <input id="value" type="number" class="form-control" name="value">
+                <span class="has-error error" style="display:none;"> </span>
+            </div>
+        </div>
     </div>
-    <div class="col-xs-6">
-        <input type="radio" name="payment_method" id="method_mc" value="cc">
-        <label for="method_mc">
-            <img src="/assets/portal/img/thumbs/mastercard.jpg" alt="" border="0"> MasterCard
-        </label>
+    <div class="row tax">
+        <div class="col-xs-6">Taxa</div>
+        <div class="col-xs-6"><input type="text" id="tax" disabled="disabled" value="0.00"></div>
     </div>
-    <div class="col-xs-6">
-        <input type="radio" name="payment_method" id="method_mb" value="mb">
-        <label for="method_mb">
-            <img src="/assets/portal/img/thumbs/mb.jpg" alt="" border="0"> Multibanco
-        </label>
+    <div class="row total">
+        <div class="col-xs-6">Total</div>
+        <div class="col-xs-6"><input type="text" id="total" disabled="disabled" value="0.00"></div>
     </div>
-    <div class="col-xs-6">
-        <input type="radio" name="payment_method" id="method_meo_wallet" value="meo_wallet">
-        <label for="method_meo_wallet">
-            <img src="/assets/portal/img/thumbs/wallet.jpg" alt="" border="0"> Meo Wallet
-        </label>
-    </div>
-    <div class="col-xs-6">
-        <input type="radio" name="payment_method" id="method_tb" value="tb">
-        <label for="method_tb">
-            <img src="/assets/portal/img/thumbs/trans_bank.jpg" alt="" border="0"> Transf. Bancária
-        </label>
-    </div>
-</div>
 
-            <input style="margin-right:30px;margin-top:70px" type="submit"  value="Confirmar" />
-
-
-
-
+    <input type="submit" value="Depositar" />
 {!! Form::close() !!}
