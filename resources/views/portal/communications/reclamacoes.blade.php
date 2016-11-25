dbf02de909e1891dea875a26fe1c42fd6ddcafa1
José Miguel Couto <jose.couto@sfponline.pt>
2016-11-25 10:16:40 +0000

    ref #94: Fix Communications page.

--- resources/views/portal/communications/reclamacoes.blade.php
+++ resources/views/portal/communications/reclamacoes.blade.php
@@ -7,61 +7,54 @@
 
 @section('sub-content')
 
-    <div class="center" style="height:500px;overflow-y:auto;overflow-x:hidden">
-        <div class="title">
-            Histórico Reclamações
-        </div>
-        <div class="bs-wp">
-
-            <table class="table table-striped" style="width:100%">
-                <thead>
-                <tr>
-                    <th style="color: #4a7fb3;font-size:14px;">Data</th>
-                    <th style="color: #4a7fb3;font-size:14px;">Situação</th>
-                    <th style="color: #4a7fb3;font-size:14px;">Assunto</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
-                /** @var $session \App\UserSession */
-                ?>
-                @foreach($complaints as $complaint)
-                    <tr onclick="showcomplain(this)">
-                        <td style="color: #4a7fb3;font-size:12px;">{{$complaint->data}}</td>
-                        <td style="color: #4a7fb3;font-size:12px;">{{$complaint->result}}</td>
-                        <td style="color: #4a7fb3;font-size:12px;">{{$complaint->complaint}}</td>
-
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="title">
+                Histórico Reclamações
+            </div>
+            <div class="wrap-table">
+                <table class="table table-striped" style="width:100%">
+                    <thead>
+                    <tr>
+                        <th style="color: #4a7fb3;font-size:14px;">Data</th>
+                        <th style="color: #4a7fb3;font-size:14px;">Situação</th>
+                        <th style="color: #4a7fb3;font-size:14px;">Assunto</th>
                     </tr>
-                    <tr id="{{$complaint->id}}" style="display:none;border:1px solid #4a7fb3">
+                    </thead>
+                    <tbody>
+                    @foreach($complaints as $complaint)
+                        <tr onclick="showcomplain(this)">
+                            <td style="color: #4a7fb3;font-size:12px;">{{$complaint->data}}</td>
+                            <td style="color: #4a7fb3;font-size:12px;">{{$complaint->result}}</td>
+                            <td style="color: #4a7fb3;font-size:12px;">{{$complaint->complaint}}</td>
 
-                        <td colspan="3">
-                            <div class="texto" style="color:#4a7fb3">{{$complaint->updated_at}}&nbsp; staff
-                                escreveu:
-                            </div>
-                            <div class="texto">{{$complaint->solution}}</div>
-                        </td>
+                        </tr>
+                        <tr id="{{$complaint->id}}" style="display:none;border:1px solid #4a7fb3">
 
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </div>
+                            <td colspan="3">
+                                <div class="texto" style="color:#4a7fb3">{{$complaint->updated_at}}&nbsp; staff
+                                    escreveu:
+                                </div>
+                                <div class="texto">{{$complaint->solution}}</div>
+                            </td>
 
-
-        <div class="title">
-            Exponha o seu problema
-        </div>
-        <div class="box">
-            <div class="box-body">
-
-
-                <div class="col-xs-12">
-                    @include('partials.input-text', ['field' => 'reclamacao', 'icon' => '', 'required' => true])
-                </div>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
-        <div class="clear"></div>
+    </div>
 
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="title">
+                Exponha o seu problema
+            </div>
+        </div>
+        <div class="col-xs-12">
+            @include('portal.partials.input-text-area', ['field' => 'reclamacao', 'icon' => '', 'required' => true])
+        </div>
     </div>
 @stop
 
@@ -74,6 +67,4 @@
     </script>
     {!! HTML::script(URL::asset('/assets/portal/js/jquery.validate.js')); !!}
     {!! HTML::script(URL::asset('/assets/portal/js/jquery.validate-additional-methods.js')); !!}
-    {!! HTML::script(URL::asset('/assets/portal/js/plugins/jquery-form/jquery.form.min.js')); !!}
-    {!! HTML::script(URL::asset('/assets/portal/js/forms.js')); !!}
 @stop
