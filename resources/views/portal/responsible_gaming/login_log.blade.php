6ef1784f9842919f591e16ea6b4468b4fa7899fd
José Miguel Couto <jose.couto@sfponline.pt>
2016-11-28 16:21:01 +0000

    ref #94: Fix Ultimos Accessos.

--- resources/views/portal/responsible_gaming/login_log.blade.php
+++ resources/views/portal/responsible_gaming/login_log.blade.php
@@ -5,30 +5,31 @@
 
 @section('sub-content')
 
-    <div class="bs-wp">
-
-        <table class="table table-striped" style="width:90%">
-            <thead>
-            <tr>
-                <th style="color: #4a7fb3;font-size:14px;">Data</th>
-                <th style="color: #4a7fb3;font-size:14px;">Tipo</th>
-                <th style="color: #4a7fb3;font-size:14px;">IP</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-            /** @var $session \App\UserSession */
-            ?>
-            @foreach($sessions as $session)
+    <div class="row">
+        <div class="col-xs-12">
+            <table class="table table-striped">
+                <thead>
                 <tr>
-                    <td style="color: #4a7fb3;font-size:12px;">{{$session->created_at->format('Y-m-d H:i')}}</td>
-                    <td style="color: #4a7fb3;font-size:12px;">{{trans('sessions_types.'.$session->session_type)}}</td>
-                    <td style="color: #4a7fb3;font-size:12px;">{{$session->ip}}</td>
-
+                    <th style="color: #4a7fb3;font-size:14px;">Data</th>
+                    <th style="color: #4a7fb3;font-size:14px;">Tipo</th>
+                    <th style="color: #4a7fb3;font-size:14px;">IP</th>
                 </tr>
-            @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                <?php
+                /** @var $session \App\UserSession */
+                ?>
+                @foreach($sessions as $session)
+                    <tr>
+                        <td style="color: #4a7fb3;font-size:12px;">{{$session->created_at->format('Y-m-d H:i')}}</td>
+                        <td style="color: #4a7fb3;font-size:12px;">{{trans('sessions_types.'.$session->session_type)}}</td>
+                        <td style="color: #4a7fb3;font-size:12px;">{{$session->ip}}</td>
+
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @stop
 
