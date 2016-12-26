e54d6705eeb073a90eadfac78dd1024da609b4db
José Miguel Couto <jose.couto@sfponline.pt>
2016-12-26 17:59:13 +0000

    ref #99: Fix footer layout.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,34 +1,39 @@
 <div class="page-footer bs-wp">
     <div class="content">
-        <div class="menu">
-            <div class="social">
-                <a target="_blank" href="https://www.facebook.com/Bet-Portugal-1753930794824917"><img src="/assets/portal/img/footer/facebook.jpg" alt="facebook"></a>
-                <a target="_blank" href="https://twitter.com/bet_portugal"><img src="/assets/portal/img/footer/twitter.jpg" alt="twitter"></a>
-                <a target="_blank" href="https://www.instagram.com/bet.portugal"><img src="/assets/portal/img/footer/instagram.jpg" alt="instagram"></a>
-                <a target="_blank" href="https://plus.google.com/u/0/b/107120429026617896567"><img src="/assets/portal/img/footer/googleplus.jpg" alt="google+"></a>
-            </div>
-            <div class="links">
-                <div><a href="/info/sobre_nos" title="Sobre Nós">Sobre Nós</a></div>
-                <div><a target="_blank" href="http://afiliados.casinoportugal.pt/" title="Afiliados">Afiliados</a></div>
-                <div><a href="/info/faq" title="FAQ">FAQ</a></div>
-                <div><a href="/info/termos_e_condicoes" title="Termos e Condições">Termos e Condições</a></div>
-                <div><a href="/info/bonus_e_promocoes" title="Bónus e Promoções">Bónus e Promoções</a></div>
-                <div><a href="/info/jogo_responsavel" title="Jogo Responsável">Jogo Responsável</a></div>
-                <div><a href="/info/politica_privacidade" title="Politica de Privacidade">Politica de Privacidade</a></div>
-                <div><a href="/info/pagamentos" title="Pagamentos">Pagamentos</a></div>
-                <div><a href="/info/contactos" title="Contactos">Contactos</a></div>
-            </div>
-            <div class="icons">
-                <a href="#"><img src="/assets/portal/img/footer/adults.jpg" alt="maiores de 18 anos"></a>
-                <a href="#"><img src="/assets/portal/img/footer/nmi.jpg" alt="NMI"></a>
-                <a href="#"><img src="/assets/portal/img/footer/srij.jpg" alt="SRIJ"></a>
+        <div class="row">
+            <div class="menu">
+                <div class="social">
+                    <a target="_blank" href="https://www.facebook.com/Bet-Portugal-1753930794824917"><img src="/assets/portal/img/footer/facebook.jpg" alt="facebook"></a>
+                    <a target="_blank" href="https://twitter.com/bet_portugal"><img src="/assets/portal/img/footer/twitter.jpg" alt="twitter"></a>
+                    <a target="_blank" href="https://www.instagram.com/bet.portugal"><img src="/assets/portal/img/footer/instagram.jpg" alt="instagram"></a>
+                    <a target="_blank" href="https://plus.google.com/u/0/b/107120429026617896567"><img src="/assets/portal/img/footer/googleplus.jpg" alt="google+"></a>
+                </div>
+                <div class="links">
+                    <div><a href="/info/sobre_nos" title="Sobre Nós">Sobre Nós</a></div>
+                    <div><a target="_blank" href="http://afiliados.casinoportugal.pt/" title="Afiliados">Afiliados</a></div>
+                    <div><a href="/info/faq" title="FAQ">FAQ</a></div>
+                    <div><a href="/info/termos_e_condicoes" title="Termos e Condições">Termos e Condições</a></div>
+                    <div><a href="/info/bonus_e_promocoes" title="Bónus e Promoções">Bónus e Promoções</a></div>
+                    <div><a href="/info/jogo_responsavel" title="Jogo Responsável">Jogo Responsável</a></div>
+                    <div><a href="/info/politica_privacidade" title="Politica de Privacidade">Politica de Privacidade</a></div>
+                    <div><a href="/info/pagamentos" title="Pagamentos">Pagamentos</a></div>
+                    <div><a href="/info/contactos" title="Contactos">Contactos</a></div>
+                </div>
+                <div class="icons">
+                    <a href="#"><img src="/assets/portal/img/footer/adults.jpg" alt="maiores de 18 anos"></a>
+                    <a href="#"><img src="/assets/portal/img/footer/nmi.jpg" alt="NMI"></a>
+                    <a href="#"><img src="/assets/portal/img/footer/srij.jpg" alt="SRIJ"></a>
+                </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <p>A BetPortugal encontra-se licenciada e regulada pelo Governo de Portugal e opera ao abrigo da Licença de Jogo Nº ###/## efectuando exploração de apostas e jogos de fortuna e azar on-line onde o operador está sujeito a monitorização e inspecção do SRIJ.</p>
+                <p>Ao aceder, continuar a utilizar ou a navegar nesta página web, aceita que utilizemos certos cookies de navegador com o objectivo de melhorar a sua experiência. A BetPortugal apenas usa cookies que melhoram a sua experiência e não interferem com a sua privacidade. Queira por favor aceder à nossa Política de Utilização para mais informação relativamente à forma como utilizamos cookies e como pode desactivar ou gerir os mesmos, caso deseje fazê-lo.</p>
 
-        <p>A BetPortugal encontra-se licenciada e regulada pelo Governo de Portugal e opera ao abrigo da Licença de Jogo Nº ###/## efectuando exploração de apostas e jogos de fortuna e azar on-line onde o operador está sujeito a monitorização e inspecção do SRIJ.</p>
-        <p>Ao aceder, continuar a utilizar ou a navegar nesta página web, aceita que utilizemos certos cookies de navegador com o objectivo de melhorar a sua experiência. A BetPortugal apenas usa cookies que melhoram a sua experiência e não interferem com a sua privacidade. Queira por favor aceder à nossa Política de Utilização para mais informação relativamente à forma como utilizamos cookies e como pode desactivar ou gerir os mesmos, caso deseje fazê-lo.</p>
-
-        <p class="copyright">SFP Online S.A. © 2016. Todos os direitos reservados.</p>
-        <div class="payments"><img src="/assets/portal/img/footer/payments.jpg" alt="pagamentos"></div>
+                <p class="copyright">SFP Online S.A. © 2016. Todos os direitos reservados.</p>
+                <div class="payments"><img src="/assets/portal/img/footer/payments.jpg" alt="pagamentos"></div>
+            </div>
+        </div>
     </div>
 </div>
