29b943c1596122b2e76a569795ba915ee34e2791
José Miguel Couto <jose.couto@sfponline.pt>
2016-12-27 16:03:08 +0000

    ref #99: Fixed footer layout.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -9,15 +9,21 @@
                     <a target="_blank" href="https://plus.google.com/u/0/b/107120429026617896567"><img src="/assets/portal/img/footer/googleplus.jpg" alt="google+"></a>
                 </div>
                 <div class="links">
-                    <div><a href="/info/sobre_nos" title="Sobre Nós">Sobre Nós</a></div>
-                    <div><a target="_blank" href="http://afiliados.casinoportugal.pt/" title="Afiliados">Afiliados</a></div>
-                    <div><a href="/info/faq" title="FAQ">FAQ</a></div>
-                    <div><a href="/info/termos_e_condicoes" title="Termos e Condições">Termos e Condições</a></div>
-                    <div><a href="/info/bonus_e_promocoes" title="Bónus e Promoções">Bónus e Promoções</a></div>
-                    <div><a href="/info/jogo_responsavel" title="Jogo Responsável">Jogo Responsável</a></div>
-                    <div><a href="/info/politica_privacidade" title="Politica de Privacidade">Politica de Privacidade</a></div>
-                    <div><a href="/info/pagamentos" title="Pagamentos">Pagamentos</a></div>
-                    <div><a href="/info/contactos" title="Contactos">Contactos</a></div>
+                    <div class="col">
+                        <a href="/info/sobre_nos" title="Sobre Nós">Sobre Nós</a>
+                        <a href="/info/termos_e_condicoes" title="Termos e Condições">Termos e Condições</a>
+                        <a href="/info/politica_privacidade" title="Politica de Privacidade">Politica de Privacidade</a>
+                    </div>
+                    <div class="col">
+                        <a target="_blank" href="http://afiliados.casinoportugal.pt/" title="Afiliados">Afiliados</a>
+                        <a href="/info/bonus_e_promocoes" title="Bónus e Promoções">Bónus e Promoções</a>
+                        <a href="/info/pagamentos" title="Pagamentos">Pagamentos</a>
+                    </div>
+                    <div class="col">
+                        <a href="/info/faq" title="FAQ">FAQ</a>
+                        <a href="/info/jogo_responsavel" title="Jogo Responsável">Jogo Responsável</a>
+                        <a href="/info/contactos" title="Contactos">Contactos</a>
+                    </div>
                 </div>
                 <div class="icons">
                     <a href="#"><img src="/assets/portal/img/footer/adults.jpg" alt="maiores de 18 anos"></a>
@@ -30,7 +36,8 @@
             <div class="col-xs-12">
                 <p>O Casino Portugal encontra-se licenciado e regulado pelo Governo de Portugal e opera ao abrigo da Licença de Jogo Nº ###/## efectuando exploração de apostas e jogos de fortuna e azar on-line onde o operador está sujeito a monitorização e inspecção do SRIJ.</p>
                 <p>Ao aceder, continuar a utilizar ou a navegar nesta página web, aceita que utilizemos certos cookies de navegador com o objectivo de melhorar a sua experiência. A BetPortugal apenas usa cookies que melhoram a sua experiência e não interferem com a sua privacidade. Queira por favor aceder à nossa Política de Utilização para mais informação relativamente à forma como utilizamos cookies e como pode desactivar ou gerir os mesmos, caso deseje fazê-lo.</p>
-
+            </div>
+            <div class="col-xs-12">
                 <p class="copyright">SFP Online S.A. © 2017. Todos os direitos reservados.</p>
                 <div class="payments" title="pagamentos"></div>
             </div>
