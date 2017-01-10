84fca6e3525b402548e7b949744347eb33267d70
Miguel Teixeira <miguel.teixeira@sfponline.pt>
2017-01-10 16:59:27 +0000

    ref #109: set https & rename Bet Portugal to Casino Portugal

--- resources/assets/js/statistics/statisticsPartials.js
+++ resources/assets/js/statistics/statisticsPartials.js
@@ -4,7 +4,7 @@ Handlebars.registerPartial('statistics', '\
             <i id="statistics-close" class="fa fa-times"></i>\
             <i id="statistics-open" class="fa fa-bar-chart"></i>\
         </div>\
-        <iframe src="http://www.score24.com/statistics3/index.jsp?partner=betportugal&eventId={{id}}" style="width: 100%" height="1800" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>\
-        <p>A informação estatística disponibilizada no site deve ser usada somente como uma guia. A Bet Portugal não se responsabiliza por eventuais erros nos dados estatísticos fornecidos pela score24. </p>\
+        <iframe src="https://www.score24.com/statistics3/index.jsp?partner=betportugal&eventId={{id}}" style="width: 100%" height="1800" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>\
+        <p>A informação estatística disponibilizada no site deve ser usada somente como uma guia. A Casino Portugal não se responsabiliza por eventuais erros nos dados estatísticos fornecidos pela score24. </p>\
     </div>\
 ');
