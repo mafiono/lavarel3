85c47c01d07efde3d4d5ba27fae4e73a59e0f02b
Miguel Teixeira <miguel.teixeira@sfponline.pt>
2017-01-10 16:43:00 +0000

    ref #109: add sports icons

--- resources/assets/js/sportsMenu/sportsMenuPartials.js
+++ resources/assets/js/sportsMenu/sportsMenuPartials.js
@@ -4,7 +4,7 @@ Handlebars.registerPartial('sports_menu', '\
             <div>\
                 <div class="sport" data-sport-id="{{id}}" data-sport-name="{{name}}" data-type="sportMenu">\
                     <i class="fa fa-plus"></i>\
-                    <i class="fa fa-futbol-o" aria-hidden="true"></i> &nbsp; {{this.name}}\
+                    <i class="fa cp-{{this.id}}" aria-hidden="true"></i> &nbsp; {{this.name}}\
                 </div>\
                 <div></div>\
             </div>\
