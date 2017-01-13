fa21258f7bcabc05bc9e758e6c9cc8d653344ede
José Miguel Couto <jose.couto@sfponline.pt>
2017-01-13 11:11:00 +0000

    ref #108: Fix old names.

--- resources/views/portal/bets/markets.blade.php
+++ resources/views/portal/bets/markets.blade.php
@@ -1,6 +1,6 @@
 <div class="markets-container" @if (isset($hidden)) style="display: none" @endif>
     <div id="breadcrumb-container" class="hidden"></div>
-    <div id="homepage-container" class="hidden" style="margin-top: 8px">
+    <div id="homepage-container" class="hidden">
         <div style="font-family: 'Exo 2','Open Sans','Droid Sans',sans-serif; font-size: 18px; font-weight: bold; line-height: 46px; margin-bottom: 8px; padding-left: 10px">
             A CASA DE APOSTAS PORTUGUESA COM CASH OUT TOTAL. VENHA JOGAR!
             @if (empty($authUser))
