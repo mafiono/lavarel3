c9dd92081f7ba061b2e9c831f5f1f95949a6b531
José Miguel Couto <jose.couto@sfponline.pt>
2017-01-17 16:25:32 +0000

    ref #116: Adicionar pagina de Boas vindas.

--- app/Http/Controllers/Portal/HomeController.php
+++ app/Http/Controllers/Portal/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Portal;
 
 use App\Http\Controllers\Controller;
 use App\Models\Highlight;
+use App\Models\WellComeLog;
+use Validator;
 use View, Auth;
 use Illuminate\Http\Request;
 
@@ -40,5 +42,37 @@ class HomeController extends Controller
         $competitions = Highlight::competitions()->get(['highlight_id']);
 
         return view('portal.index',  compact("phpAuthUser", "competitions"));
+    }
+
+    /**
+     * @return View
+     */
+    public function wellcome()
+    {
+        $msg = '';
+        $error = null;
+        if ($this->request->isMethod('POST')) {
+            try {
+                $validator = Validator::make($this->request->all(), [
+                    'email' => 'required|email|unique:wellcome,email',
+                ], [
+                    'email.required' => 'Indique um email por favor',
+                    'email.email' => 'Indique um email válido por favor',
+                    'email.unique' => 'Este email já está subscrito'
+                ]);
+                if (!$validator->fails()) {
+                    $entry = new WellComeLog();
+                    $entry->email = $this->request->get('email');
+                    if ($entry->save()) {
+                        $msg = 'Gravado com sucesso';
+                    }
+                } else {
+                    $error = $validator->errors()->get('email')[0];
+                }
+            } catch (\Exception $e) {
+                $error = $e->getMessage();
+            }
+        }
+        return view('portal.wellcome-page', compact('msg', 'error'));
     }
 }
