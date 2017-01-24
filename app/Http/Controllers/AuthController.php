7e2f0779a72f686c1c12cdf6daf202344c097988
José Miguel Couto <jose.couto@sfponline.pt>
2017-01-24 15:20:57 +0000

    ref #117: Added status of SRIJ service to DB.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -148,8 +148,12 @@ class AuthController extends Controller
             $name = $inputs['fullname'];
             if (!$this->validaUser($cc, $nif, $name, $date)) {
                 Session::put('identity', true);
+                $inputs['identity_checked'] = 0;
+                $inputs['identity_method'] = 'none';
             } else {
                 $identityStatus = 'confirmed';
+                $inputs['identity_checked'] = 1;
+                $inputs['identity_method'] = 'srij';
             }
         } catch (Exception $e){
             Session::put('allowStep2', true);
@@ -629,7 +633,7 @@ class AuthController extends Controller
         $identity = $ws->verificacaoidentidade($part);
         Log::info('VIdentidade', compact('name', 'cc', 'tipo', 'date', 'nif', 'identity'));
         if (!$identity->Sucesso){
-            throw new Exception($identity->CodigoErro . ': ' . $identity->MensagemErro. ' > ' . $identity->DetalheErro);
+            throw new Exception($identity->MensagemErro, $identity->CodigoErro, $identity->DetalheErro);
         }
         return $identity->Valido === 'S';
     }
