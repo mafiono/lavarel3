6a4e3dee16a8bcaa3a6d385c23cb2b468d12be0d
José Miguel Couto <jose.couto@sfponline.pt>
2017-01-26 15:57:44 +0000

    ref #114: Fix jquery-ui to be bundled.

--- gulpfile.js
+++ gulpfile.js
@@ -18,8 +18,6 @@ elixir(function(mix) {
     mix.webpack('app.js', 'public/assets/portal/js/app.js');
     mix.webpack('bundle.js', 'public/assets/portal/js/bundle.js');
 
-    mix.less('style.less', 'public/assets/portal/newstyle/style.css');
-
     mix.sass('sports.scss', 'public/assets/portal/css/sports.css');
     mix.sass('app.scss', 'public/assets/portal/css/app.css');
     mix.less('style.less', 'public/assets/portal/newStyle/style.css');
