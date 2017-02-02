5d982421b67e8f58b1f2a499611a9704b2399f79
Miguel Teixeira <miguel.teixeira@sfponline.pt>
2017-02-02 11:22:48 +0000

    ref #127: add volleyball for fixtures tables

--- resources/assets/js/sports/fixturesPartials.js
+++ resources/assets/js/sports/fixturesPartials.js
@@ -36,18 +36,18 @@ Handlebars.registerPartial('fixtures', '\
                     {{#if_in market_type_id "2,15,306,6662,7469,8133"}}\
                         {{> get_selection outcomeId=1 fixture=.. index=@../index}}\
                     {{/if_in}}\
-                    {{#if_eq market_type_id 322}}\
+                    {{#if_in market_type_id "322,6734"}}\
                         {{> get_selection outcomeId=25 fixture=.. index=@../index}}\
-                    {{/if_eq}}\
+                    {{/if_in}}\
                     <td class="separator"></td>\
                         {{> get_selection outcomeId=2 fixture=.. index=@../index}}\
                     <td class="separator"></td>\
                     {{#if_in market_type_id "2,15,306,6662,7469,8133"}}\
                         {{> get_selection outcomeId=3 fixture=.. index=@../index}}\
                     {{/if_in}}\
-                    {{#if_eq market_type_id 322}}\
+                    {{#if_in market_type_id "322,6734"}}\
                         {{> get_selection outcomeId=26 fixture=.. index=@../index}}\
-                    {{/if_eq}}\
+                    {{/if_in}}\
                 {{/each}}\
                 <td class="separator">&nbsp;</td>\
                 <td class="marketsCount {{parity @index}}" data-game-id="{{id}}" data-type="fixture">+{{markets_count}}</td>\
