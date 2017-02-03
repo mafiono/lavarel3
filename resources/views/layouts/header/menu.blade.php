d6f9717dad5425514a0a1d14168ae92ee3c2c389
Miguel Teixeira <miguel.teixeira@sfponline.pt>
2017-02-03 16:34:06 +0000

    ref #129: link only works if casino is available

--- resources/views/layouts/header/menu.blade.php
+++ resources/views/layouts/header/menu.blade.php
@@ -1,3 +1,3 @@
 <li id="header-live"><a href="/direto" title="DIRETO">DIRETO</a></li>
 <li id="header-prematch"><a href="/desportos" title="DESPORTOS">DESPORTOS</a></li>
-<li><a href="#casino" title="CASINO">CASINO</a></li>
+<li><a href="{{config('app.casino_available') ? '/casino' : 'javascript:void(0)'}}" title="CASINO">CASINO</a></li>
