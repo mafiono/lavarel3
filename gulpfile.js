38ed9a938b70c7465ad2e63ef4ba4a0d9ef9974c
Miguel Teixeira <miguel.teixeira@sfponline.pt>
2017-02-03 15:33:42 +0000

    ref #129: add vue & casino js & casino scss

--- gulpfile.js
+++ gulpfile.js
@@ -14,11 +14,15 @@ elixir.config.css.autoprefix.options.browsers = ['> 1%', 'Last 10 versions', 'IE
 
 require('laravel-elixir-webpack-official');
 
+require('laravel-elixir-vue-2');
+
 elixir(function(mix) {
     mix.webpack('app.js', 'public/assets/portal/js/app.js');
     mix.webpack('bundle.js', 'public/assets/portal/js/bundle.js');
+    mix.webpack('casino.js', 'public/assets/portal/js/casino.js');
 
     mix.sass('sports.scss', 'public/assets/portal/css/sports.css');
+    mix.sass('casino.scss', 'public/assets/portal/css/casino.css');
     mix.sass('app.scss', 'public/assets/portal/css/app.css');
     mix.less('style.less', 'public/assets/portal/newStyle/style.css');
 });
