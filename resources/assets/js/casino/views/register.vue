f989cfb2ea219fc66f622079ff2320d1b9f06bf2
Miguel Teixeira <miguel.teixeira@sfponline.pt>
2017-02-05 12:13:52 +0000

    ref #129: change register component operate outside iframe

--- resources/assets/js/casino/views/register.vue
+++ resources/assets/js/casino/views/register.vue
@@ -1,13 +1,25 @@
 <template>
-    <iframe src="/registar/step1" scrolling="no"
-        style="height: 1000px; width: 640px; border: 0; float: left;">
-    </iframe>
+    <div id="register-container"></div>
 </template>
 <script>
     export default {
+        methods: {
+            register: function() {
+                var ctx = {
+                    params: {
+                        0: "/registar",
+                        "step": null
+                    }
+                };
+                Register.make(ctx);
+            }
+        },
         mounted: function() {
-            if (userLoggedIn)
-                this.$router.replace('/');
+            if (userLoggedIn) {
+                this.$router.replace('/casino');
+
+                return;
+            }
         }
     }
 </script>
