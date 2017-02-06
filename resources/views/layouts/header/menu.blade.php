1f35329e8189aa3ce9f70cfbacc3b7a5871e7f8f
Miguel Teixeira <miguel.teixeira@sfponline.pt>
2017-02-06 15:29:03 +0000

    ref #129: fix casino link

--- resources/views/layouts/header/menu.blade.php
+++ resources/views/layouts/header/menu.blade.php
@@ -1,3 +1,5 @@
 <li id="header-live"><a href="/direto" title="DIRETO">DIRETO</a></li>
 <li id="header-prematch"><a href="/desportos" title="DESPORTOS">DESPORTOS</a></li>
-<li><a href="{{config('app.casino_available') && !$casino ? '/casino' : 'javascript:void(0)'}}" title="CASINO">CASINO</a></li>
+<li class="{{$casino ? 'active' : ''}}">
+    <a href="{{config('app.casino_available') && !$casino ? '/casino' : 'javascript:void(0)'}}" title="CASINO">CASINO</a>
+</li>
