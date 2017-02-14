c39e2caeca02c7af819b6b7a225eab879c4a9d6c
Miguel Teixeira <miguel.teixeira@sfponline.pt>
2017-02-14 11:57:31 +0000

    ref #129: add daily, weekly and monthly  amounts

--- app/Models/CasinoTransaction.php
+++ app/Models/CasinoTransaction.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Traits\CasinoDatabase;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class CasinoTransaction extends Model
@@ -19,5 +20,44 @@ class CasinoTransaction extends Model
     public function round()
     {
         return $this->belongsTo(CasinoRound::class);
+    }
+
+    public function scopeNotFreeRound($query)
+    {
+        return $query->where('fround_id', 0);
+    }
+
+    public static function dailyAmount($user_id)
+    {
+        $amount = static::where('user_id', $user_id)
+            ->whereTransactionstatus('ok')
+            ->notFreeRound()
+            ->where('created_at', '>=', Carbon::now()->startOfDay())
+            ->sum('amount');
+
+        return round($amount/100, 2);
+    }
+
+    public static function weeklyAmount($user_id)
+    {
+
+        $amount = self::where('user_id', $user_id)
+            ->whereTransactionstatus('ok')
+            ->notFreeRound()
+            ->where('created_at', '>=', Carbon::now()->startOfWeek())
+            ->sum('amount');
+
+        return round($amount/100, 2);
+    }
+
+    public static function monthlyAmount($user_id)
+    {
+        $amount = self::where('user_id', $user_id)
+            ->whereTransactionstatus('ok')
+            ->notFreeRound()
+            ->where('created_at', '>=', Carbon::now()->startOfMonth())
+            ->sum('amount');
+
+        return round($amount/100, 2);
     }
 }
