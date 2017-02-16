6cd5c73e3abb2318cd7b72c93e43f1bf68920aab
José Miguel Couto <jose.couto@sfponline.pt>
2017-02-16 09:14:20 +0000

    #138: Improve performance.

--- app/Console/Commands/BetCreatorCommand.php
+++ app/Console/Commands/BetCreatorCommand.php
@@ -31,9 +31,16 @@ class BetCreatorCommand extends Command
         $query = DB::table('betgenius.selections as s')
             ->leftJoin('betgenius.markets as m', 's.market_id', '=', 'm.id')
             ->leftJoin('betgenius.fixtures as f', 'm.fixture_id', '=', 'f.id')
+            ->leftJoin('betgenius.competitions as c', 'f.competition_id', '=', 'c.id')
+            ->leftJoin('betgenius.sports as sp', 'f.sport_id', '=', 'sp.id')
+            ->leftJoin('betgenius.regions as r', 'c.region_id', '=', 'r.id')
             ->leftJoin('betportugal.user_bet_events as be', function ($join) use($today) {
                 $join->on('be.api_event_id', '=', 's.id');
                 $join->where('be.created_at', '>', $today);
+            })
+            ->leftJoin('betportugal.user_bets as ub', function ($join) use($user) {
+                $join->on('ub.id', '=', 'be.user_bet_id');
+                $join->where('ub.user_id', '=', $user->id);
             })
             ->select([
                 's.id',
@@ -46,6 +53,8 @@ class BetCreatorCommand extends Command
                 'f.start_time_utc as gameDate',
             ])
             ->whereNull('be.id')
+            //->whereNull('c.authorized')
+            ->where('c.authorized', '=', '1')
             ->where('s.decimal', '>', 1.08)
             //->whereIn('m.market_type_id', [2, 322, 306, 6662, 7469, 8133, 15, 6734,])
             ->where('f.status', '=', 'Scheduled')
@@ -55,7 +64,18 @@ class BetCreatorCommand extends Command
             ->where('f.start_time_utc', '>', Carbon::now()->tz('UTC'))
             //->limit(10)
             ;
-        // dd($query->count());
+//        dd($query
+////            ->groupBy('c.id')
+////            ->select([
+////                'sp.name as sport', 'r.name as region', 'c.id', 'c.name', DB::raw('count(distinct f.id) as Games')
+////            ])
+////            ->orderBy(DB::raw('count(distinct f.id)'), 'desc')
+////            ->limit(10)
+//            ->select([
+//                DB::raw('count(distinct f.id) as Games')
+//            ])
+//            ->get()
+//        );
 
         $apostas = $query
             // ->limit(200)
@@ -63,10 +83,12 @@ class BetCreatorCommand extends Command
 
         $count = 0;
         $success = 0;
+        $total = count($apostas);
+        $this->line("Submetidas $total apostas!");
         foreach ($apostas as $betSel) {
             $count++;
             if ($count % 50 === 0) {
-                $this->line("Submetidas $count apostas! $success com sucesso!");
+                $this->line("Submetidas $count/$total apostas! $success com sucesso!");
             }
             try {
                 DB::transaction(function () use($betSel, $user, $sessionId) {
