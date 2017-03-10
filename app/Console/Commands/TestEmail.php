31f110daed9b76c59d29cbe3af401cdf1958a4ed
José Miguel Couto <jose.couto@sfponline.pt>
2017-03-10 17:31:02 +0000

    #170: Fix job and Links.

--- app/Console/Commands/TestEmail.php
+++ app/Console/Commands/TestEmail.php
@@ -18,7 +18,7 @@ class TestEmail extends Command
      *
      * @var string
      */
-    protected $signature = 'verify_email {id}';
+    protected $signature = 'verify_email {type} {id}';
 
     /**
      * The console command description.
@@ -35,25 +35,46 @@ class TestEmail extends Command
     public function handle()
     {
         $id = $this->argument('id');
+        $type = $this->argument('type');
+
+        if (empty($type)) $type = 'basic';
         $user = User::findById($id);
 
-        $token = $user->profile->email_token;
 
-        $data = [
+        $vars = [
+            'user' => $user,
+            'name' => $user->username,
             'email' => $user->profile->email,
-            'name' => $user->profile->name,
+            'title' => 'THIS IS A TITLE',
+            'url' => 'www.casinoportugal.pt',
+            'host' => 'https://www.casinoportugal.pt',
+            'button' => 'CONFIRMAR',
+            'value' => '20,00',
+            'nr' => '00001',
+            'exclusion' => $this->array_random(['reflection_period', 'undetermined_period', 'other']),
+            'time' => '5',
+            'motive' => 'Uso indevido!'
         ];
-        try {
-            $url = Request::getHost() . '/confirmar_email?email=' . $user->profile->email . '&token=' . $token;
 
-            Mail::send('portal.sign_up.emails.signup', ['data' => $data, 'token' => $token, 'url' => $url],
-                function ($m) use ($data) {
-                    $m->to($data['email'], $data['name'])->subject('CasinoPortugal - Registo de Utilizador!');
+        try {
+            Mail::send('emails.types.' . $type, $vars,
+                function ($m) use ($vars) {
+                    $m->to($vars['email'], $vars['name'])->subject($vars['title']);
                 });
             $this->line('Success Sending Mail!');
         } catch (Exception $e) {
             $this->error('Error sending mail: ' . $e->getMessage());
             var_dump($e->getTraceAsString());
         }
+    }
+
+    private function array_random($arr, $num = 1) {
+        shuffle($arr);
+
+        $r = array();
+        for ($i = 0; $i < $num; $i++) {
+            $r[] = $arr[$i];
+        }
+        return $num == 1 ? $r[0] : $r;
     }
 }
