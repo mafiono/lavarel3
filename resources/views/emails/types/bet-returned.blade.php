eb7197ecbaa2eaf4c97eb8b830df461df5085f6c
José Miguel Couto <jose.couto@sfponline.pt>
2017-03-13 09:18:53 +0000

    #170: Fix messages.

--- resources/views/emails/types/bet-returned.blade.php
+++ resources/views/emails/types/bet-returned.blade.php
@@ -7,7 +7,7 @@
 @section('message')
     <p>A sua aposta nº{{$nr}}, no valor de {{$value}}€, foi devolvida por motivos alheios ao Casino Portugal. </p>
     <p>&nbsp;</p>
-    <p>Caso não tenho solicitado este levantamento contacte o nosso <a href="mailto:apoiocliente@casinoportugal.pt" class="link">apoio ao cliente</a>.</p>
+    <p>Para mais informações contacte o nosso <a href="mailto:apoiocliente@casinoportugal.pt" class="link">apoio ao cliente</a>.</p>
     <p>&nbsp;</p>
     @include('emails.layouts.welcome')
 @endsection
--- resources/views/emails/types/fail-login.blade.php
+++ resources/views/emails/types/fail-login.blade.php
@@ -7,7 +7,7 @@
 @section('message')
     <p>Detetámos uma tentativa de acesso à sua conta Casino Portugal sem sucesso.</p>
     <p>&nbsp;</p>
-    <p>Caso não tenho solicitado este levantamento contacte o nosso <a href="mailto:apoiocliente@casinoportugal.pt" class="link">apoio ao cliente</a>.</p>
+    <p>Caso não tenha efetuado este pedido, entre em contacto com o nosso <a href="mailto:apoiocliente@casinoportugal.pt" class="link">apoio ao cliente</a>.</p>
     <p>&nbsp;</p>
     <p>Caso deseje definir uma nova palavra-passe aceda a este link:</p>
     <p>&nbsp;</p>
