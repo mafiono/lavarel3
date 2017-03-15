91cbd6d9bc77e1fa5381d0b5bf317002bd6d792c
Diogo Araujo @ SFPONLINE <diogo.araujo@amorimturismo.corp>
2017-03-15 18:02:58 +0000

    fix

---
+++ database/migrations/2017_03_15_180153_add_odd_to_daily.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddOddToDaily extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('daily_bet', function (Blueprint $table) {
+            $table->decimal('odd');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('daily_bet', function (Blueprint $table) {
+            $table->removeColumn('odd');
+        });
+    }
+}
