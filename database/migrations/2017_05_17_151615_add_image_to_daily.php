4acd7e6dac798e553d31447d686bf3ac3c0c0c9d
Diogo Araujo @ SFPONLINE <diogo.araujo@amorimturismo.corp>
2017-05-17 15:19:05 +0100

    #223 Add image to daily_bet

---
+++ database/migrations/2017_05_17_151615_add_image_to_daily.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddImageToDaily extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('daily_bet', function (Blueprint $table) {
+            $table->binary('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('daily_bet', function (Blueprint $table) {
+            $table->removeColumn('image');
+        });
+    }
+}
