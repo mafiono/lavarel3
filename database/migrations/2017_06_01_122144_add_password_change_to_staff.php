4de366b07d41c2aa8459136fe3c4201f9c44dcac
José Miguel Couto <jose.couto@sfponline.pt>
2017-06-02 10:44:13 +0100

    #250: Added Migration of Highlighs.

--- database/migrations/2017_06_01_122144_add_password_change_to_staff.php
+++ database/migrations/2017_06_01_122144_add_password_change_to_staff.php
@@ -25,7 +25,7 @@ class AddPasswordChangeToStaff extends Migration
     public function down()
     {
         Schema::table('user_notes', function (Blueprint $table) {
-            $table->removeColumn('changedpassword');
+            $table->dropColumn('changedpassword');
         });
     }
 }
--- database/migrations/2017_06_02_104013_add_priority_highlights.php
+++ database/migrations/2017_06_02_104013_add_priority_highlights.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddBicToUserBankAccounts extends Migration
+class AddPriorityHighlights extends Migration
 {
     /**
      * Run the migrations.
@@ -12,8 +12,8 @@ class AddBicToUserBankAccounts extends Migration
      */
     public function up()
     {
-        Schema::table('user_bank_accounts', function (Blueprint $table) {
-            $table->string('bank_bic', 15)->nullable()->after('bank_account');
+        Schema::table('highlights', function (Blueprint $table) {
+            $table->integer('priority');
         });
     }
 
@@ -24,8 +24,8 @@ class AddBicToUserBankAccounts extends Migration
      */
     public function down()
     {
-        Schema::table('user_bank_accounts', function (Blueprint $table) {
-            $table->dropColumn('bank_bic');
+        Schema::table('highlights', function (Blueprint $table) {
+            $table->dropColumn('priority');
         });
     }
 }
