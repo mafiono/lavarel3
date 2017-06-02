9100f3d41de314c964a9541bcc36d1883f44f2f8
José Miguel Couto <jose.couto@sfponline.pt>
2017-06-02 09:54:07 +0100

    #253: Fix total.

--- app/Http/Controllers/Portal/HistoryController.php
+++ app/Http/Controllers/Portal/HistoryController.php
@@ -64,7 +64,7 @@ class HistoryController extends Controller {
                 'user_bets.`api_bet_id` as `description`, ' .
                 'user_bets.status,' .
                 'ubt.operation,' .
-                'ubt.final_balance,' .
+                'CONVERT(ubt.`final_balance` + ubt.`final_bonus`, DECIMAL(15,2)) as `final_balance`,' .
                 'CONVERT(ubt.`amount_balance` + ubt.`amount_bonus`, DECIMAL(15,2)) as `value`,' .
                 'CONVERT(IFNULL(user_bets.`amount_taxed`, 0), DECIMAL(15,2)) as `tax`'));
 
