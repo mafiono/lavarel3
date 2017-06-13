c191b5f5d440ed93ab48507056b4a5a0f1599372
José Miguel Couto <jose.couto@sfponline.pt>
2017-06-13 16:30:02 +0100

    #265: Alterar Texto do elapsed nos jogos Live.

--- resources/assets/js/sportsMenu/fixturesMenuPartials.js
+++ resources/assets/js/sportsMenu/fixturesMenuPartials.js
@@ -3,7 +3,7 @@ Handlebars.registerPartial('fixtures_menu','\
         <div class="fixture" data-sport-id="{{../sportId}}" data-sport-name="{{../sportName}}" data-region-id="{{../regionId}}" data-region-name="{{../regionName}}" data-game-id="{{id}}" data-game-name="{{name}}" data-type="fixtureMenu">\
             <div class="favorite">{{> favorite}}</div>\
             <div class="game" data-game-id="{{id}}" data-mode="live" title="{{name}}">{{name}}</div>\
-            <div class="matchState">{{elapsed}}\'<br>{{score}}</div>\
+            <div class="matchState">{{elapsed}}<br>{{score}}</div>\
         </div>\
     {{/each}}\
 ');
