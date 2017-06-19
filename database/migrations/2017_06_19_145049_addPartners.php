db8d0b4af71efdbc82cfbeff144e0fb10c6edd2a
Diogo.Araujo <diogo.araujo@sfponline.pt>
2017-06-19 14:54:04 +0100

    #271 Partners Migration

---
+++ database/migrations/2017_06_19_145049_addPartners.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPartners extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+        public function up()
+    {
+        Schema::create('partners', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->boolean('active');
+            $table->timestamps();
+        });
+        Schema::create('ads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('partner_id')->unsigned();
+            $table->string('link', 250)->nullable();
+            $table->integer('clicks')->nullable();
+            $table->timestamps();
+            $table->foreign('partner_id')->references('id')->on('partners');
+        });
+    }
+
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+         */
+        public function down()
+    {
+        Schema::drop('banners_frames');
+        Schema::drop('banners');
+    }
+
+}
