64aa3ad7b0624d4461c58427394f4e82b5278a88
Miguel Teixeira <miguel.teixeira@sfponline.pt>
2017-06-26 19:59:37 +0100

    wip

--- app/Http/Controllers/PromotionsController.php
+++ app/Http/Controllers/PromotionsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Promotion;
 use App\UserBet;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class PromotionsController extends Controller
@@ -28,12 +29,14 @@ class PromotionsController extends Controller
 
     }
 
-    public function bigodd()
+    public function bigodd(Request $request)
     {
         $position = 1;
 
         return UserBet::whereType('multi')
             ->where('amount', '>=', 5)
+            ->whereResult('won')
+            ->whereMonth('updated_at', '=', $request->month ?? Carbon::now()->month)
             ->whereDoesntHave('events', function ($query) {
                 return $query->where('odd', '<', 1.3);
             })->orderBy('odd', 'desc')
--- resources/assets/js/common/components/promotions-bigodd.vue
+++ resources/assets/js/common/components/promotions-bigodd.vue
@@ -20,42 +20,6 @@
         </tr>
 
     </table>
-
-    <!--<table border="0" cellpadding="1" cellspacing="1" style="width: 600px;">-->
-        <!--<tbody>-->
-        <!--<tr>-->
-            <!--<td colspan="5" align="right" bgcolor="#2D415C"><span class="white">&nbsp;</span><span class="white">&nbsp;</span><span class="white">&nbsp;</span><span class="white">&nbsp;</span><span class="white">&nbsp;</span></td>-->
-        <!--</tr>-->
-        <!--<tr>-->
-            <!--<td width="49" align="center" bgcolor="#2D415C">&nbsp;</td>-->
-            <!--<td width="193" align="left" bgcolor="#2D415C" class="titulo">Utilizador</td>-->
-            <!--<td width="163" align="center" bgcolor="#2D415C" class="titulo">Odd</td>-->
-            <!--<td width="159" align="right" bgcolor="#2D415C" class="titulo">Montante</td>-->
-            <!--<td width="20" rowspan="4" align="right" bgcolor="#2D415C"><span class="white">&nbsp;</span><span class="white">&nbsp;</span><span class="white">&nbsp;</span><span class="white">&nbsp;</span></td>-->
-        <!--</tr>-->
-        <!--<tr>-->
-            <!--<td align="center" bgcolor="#2D415C"><span class="yellow">1º</span></td>-->
-            <!--<td align="left" bgcolor="#2D415C"><span class="white">Marauder</span></td>-->
-            <!--<td align="center" bgcolor="#2D415C"><span class="white">20.50</span></td>-->
-            <!--<td align="right" bgcolor="#2D415C"><span class="white">500€</span></td>-->
-        <!--</tr>-->
-        <!--<tr>-->
-            <!--<td align="center" bgcolor="#2D415C"><span class="yellow">2º</span></td>-->
-            <!--<td align="left" bgcolor="#2D415C"><span class="white">Jaquim</span></td>-->
-            <!--<td align="center" bgcolor="#2D415C"><span class="white">19.23</span></td>-->
-            <!--<td align="right" bgcolor="#2D415C"><span class="white">450€</span></td>-->
-        <!--</tr>-->
-        <!--<tr>-->
-            <!--<td align="center" bgcolor="#2D415C"><span class="yellow">3º</span></td>-->
-            <!--<td align="left" bgcolor="#2D415C"><span class="white">Toino</span></td>-->
-            <!--<td align="center" bgcolor="#2D415C"><span class="white">9.45</span></td>-->
-            <!--<td align="right" bgcolor="#2D415C"><span class="white">345€</span></td>-->
-        <!--</tr>-->
-        <!--<tr>-->
-            <!--<td colspan="5" align="right" bgcolor="#2D415C"><span class="white">&nbsp;</span><span class="white">&nbsp;</span><span class="white">&nbsp;</span><span class="white">&nbsp;</span><span class="white">&nbsp;</span></td>-->
-        <!--</tr>-->
-        <!--</tbody>-->
-    <!--</table>-->
 </template>
 <script>
     export default {
@@ -69,8 +33,9 @@
                 return `${number_format(value, 2, '.', ' ')}€`;
             }
         },
+        props: ['month'],
         mounted() {
-            $.get("/promotions/bigodd", (data) => {
+            $.get("/promotions/bigodd" + this.month+"?month=", (data) => {
                 this.tops = data;
             });
         }
