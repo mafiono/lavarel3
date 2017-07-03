df756f8c60407b673b91f657dbaa3e6f042c7a14
Diogo.Araujo <diogo.araujo@sfponline.pt>
2017-07-03 16:34:05 +0100

    #324 add tracker

--- resources/views/portal/sign_up/step_2.blade.php
+++ resources/views/portal/sign_up/step_2.blade.php
@@ -66,3 +66,21 @@
         @endif
     </div>
 @stop
+
+<script type="text/javascript">
+    / <![CDATA[ /
+    var google_conversion_id = 860696449;
+    var google_conversion_language = "en";
+    var google_conversion_format = "3";
+    var google_conversion_color = "ffffff";
+    var google_conversion_label = "wUFBCKC3wHIQgd-0mgM";
+    var google_remarketing_only = false;
+    / ]]> /
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+    <div style="display:inline;">
+        <img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/860696449/?label=wUFBCKC3wHIQgd-0mgM&guid=ON&script=0"/>
+    </div>
+</noscript>
