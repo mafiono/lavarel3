15ce2a275c9400d290d2364aadbf8aea9eb003d0
José Miguel Couto <jose.couto@sfponline.pt>
2017-07-14 14:13:36 +0100

    #336: Fix problem with History not working in DEV/QUA dbs.

--- app/Bets/Models/Selection.php
+++ app/Bets/Models/Selection.php
@@ -2,11 +2,12 @@
 
 namespace App\Bets\Models;
 
+use App\Traits\OddsDatabase;
 use Illuminate\Database\Eloquent\Model;
 
 class Selection extends Model
 {
-    protected $connection = 'odds';
+    use OddsDatabase;
 
     public function result()
     {
