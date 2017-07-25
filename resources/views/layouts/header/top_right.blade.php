c9ae0df13e9dea550ec4cc30a0f1be565090a255
Diogo.Araujo <diogo.araujo@sfponline.pt>
2017-07-25 12:41:47 +0100

    #343 fix clock

--- resources/views/layouts/header/top_right.blade.php
+++ resources/views/layouts/header/top_right.blade.php
@@ -16,7 +16,7 @@
     <div class="user-time hide" {{Session::has('user_login_time') ? 'data-time=' . Session::get('user_login_time') .'000': '' }}>
         <i class="cp-clock"></i> <span></span>
     </div>
-    <div class="server-time" data-time="{{Carbon\Carbon::now()->getTimestamp()}}000"></div>
+    <div class="server-time" data-time="{{Carbon\Carbon::now()->getTimestamp()}} GMT+1"></div>
 </div>
 
 
