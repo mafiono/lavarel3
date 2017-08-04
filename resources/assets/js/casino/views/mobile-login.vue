d1ff2c5e56edda7f8f584efff59995adfbd122c7
Miguel Teixeira <miguel.teixeira@sfponline.pt>
2017-08-04 15:37:53 +0100

    ref #330: redirect login view

--- resources/assets/js/casino/views/mobile-login.vue
+++ resources/assets/js/casino/views/mobile-login.vue
@@ -2,9 +2,13 @@
     <mobile-login context="casino"></mobile-login>
 </template>
 <script>
-    export default{
+    export default {
         components: {
             'mobile-login': require('../../common/components/mobile-login.vue'),
+        },
+        mounted() {
+            if(Store.getters['user/isAuthenticated'])
+                router.replace('/');
         }
     }
 </script>
