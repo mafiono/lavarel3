d40db4ff496cd1c3cfa31a54bddfba5222f7c7fa
Diogo.Araujo <diogo.araujo@sfponline.pt>
2017-08-04 11:32:43 +0100

    #356 ad Migration

---
+++ database/migrations/2017_08_04_112824_addAdName.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAdName extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ads', function (Blueprint $table) {
+            $table->string('name')->nullable();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ads', function (Blueprint $table) {
+            $table->dropColumn('name');
+        });
+    }
+}
