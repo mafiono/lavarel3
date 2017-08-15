a1efe1587ce50d2db66094818d63f76fa6b2a11e
Miguel Teixeira <miguel.teixeira@sfponline.pt>
2017-08-15 12:57:41 +0100

    ref: #363 add nobonus & deposit bonus classes

---
+++ app/Bonus/Casino/Deposit.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Bonus\Casino;
+
+
+class Deposit extends BaseCasinoBonus
+{
+
+}
---
+++ app/Bonus/Casino/NoBonus.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Bonus\Casino;
+
+
+class NoBonus extends BaseCasinoBonus
+{
+
+}
