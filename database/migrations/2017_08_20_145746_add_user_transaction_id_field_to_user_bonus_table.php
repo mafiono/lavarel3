5d18d935fe576f6b7e8c70a3bc5cddb57e1697ef
Miguel Teixeira <miguel.teixeira@sfponline.pt>
2017-08-20 15:00:53 +0100

    ref: #363 AddUserTransactionIdFieldToUserBonusTable for later use.

---
+++ database/migrations/2017_08_20_145746_add_user_transaction_id_field_to_user_bonus_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserTransactionIdFieldToUserBonusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_bonus', function (Blueprint $table) {
+            $table->integer('user_transaction_id')->unsigned()->nullable();
+
+            $table->foreign('user_transaction_id')->references('id')->on('user_transactions');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_bonus', function (Blueprint $table) {
+            $table->dropColumn('user_transaction_id');
+        });
+    }
+}
