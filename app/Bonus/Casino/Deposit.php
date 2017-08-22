19c65fee69523c6a65387f68ec5c73c7a06e00ae
Miguel Teixeira <miguel.teixeira@sfponline.pt>
2017-08-22 12:06:56 +0100

    ref: #363 deposit

--- app/Bonus/Casino/Deposit.php
+++ app/Bonus/Casino/Deposit.php
@@ -2,7 +2,35 @@
 
 namespace App\Bonus\Casino;
 
+use App\UserTransaction;
+use Carbon\Carbon;
+
 class Deposit extends BaseCasinoBonus
 {
+    public function deposit()
+    {
+        $bonusAmount = $this->redeemAmount();
 
+        $initial_bonus = $this->user->balance->balance_bonus;
+        $this->user->balance->addBonus($bonusAmount);
+
+        $this->userBonus->update([
+            'bonus_value' => $bonusAmount,
+            'deposited' => 1,
+        ]);
+
+        UserTransaction::forceCreate([
+            'user_id' => $this->user->id,
+            'origin' => 'casino_bonus',
+            'transaction_id' => UserTransaction::getHash($this->user->id, Carbon::now()),
+            'debit_bonus' => $bonusAmount,
+            'initial_balance' => $this->user->balance->balance_available,
+            'initial_bonus' => $initial_bonus,
+            'final_balance' => $this->user->balance->balance_available,
+            'final_bonus' => $this->user->balance->balance_bonus,
+            'date' => Carbon::now(),
+            'description' => 'Resgate de bónus ' . $this->userBonus->bonus->title,
+            'status_id' => 'processed',
+        ]);
+    }
 }
