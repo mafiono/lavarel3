c9d071edb204b84c944b07d5f29aa669431655a6
José Miguel Couto <jose.couto@sfponline.pt>
2017-08-28 16:46:25 +0100

    #364: Added Extra chars as valid.

--- app/Http/Middleware/Affiliates.php
+++ app/Http/Middleware/Affiliates.php
@@ -19,9 +19,12 @@ class Affiliates
     {
         if (isset($_GET['btag'])) {
             $btag = $_GET['btag'];
-            $btag = preg_match('/^(\d{3,19})([_\-.])(\d{1,20})/', $btag, $matches);
-            if ($btag && count($matches) > 3) {
-                $btag = $matches[1] . '_' . $matches[3];
+            $btag = preg_match('/^(\d{3,19})(([_\-.])(\d{1,20}))?/', $btag, $matches);
+            if ($btag && count($matches) > 1) {
+                $btag = $matches[1];
+                if (count($matches)>3) {
+                    $btag .= '_' . $matches[4];
+                }
                 Cookie::queue('btag', $btag, 45000);
             }
         }
