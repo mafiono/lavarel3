de372e283c74f4bf479cfd6195e4cc068ab5e861
Miguel Teixeira <miguel.teixeira@sfponline.pt>
2017-09-29 18:15:34 +0100

    ref #412: fix cookies consent

--- resources/assets/js/casino.js
+++ resources/assets/js/casino.js
@@ -179,6 +179,7 @@ new Vue({
         'mobile-header': require('./common/components/mobile-header.vue'),
         'mobile-menu': require('./common/components//mobile-menu.vue'),
         'mobile-up-button': require('./common/components/mobile-up-button.vue'),
+        'cookies-consent': require('./common/components/cookies-consent.vue'),
     },
     router,
     watch: {
