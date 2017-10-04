f34c35f48e35e0477084ab625e084c1ad3720178
Diogo.Araujo <diogo.araujo@sfponline.pt>
2017-10-04 11:11:39 +0100

    #414 fix

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -16,16 +16,12 @@ class ApiController extends Controller
     protected function userHasPromoCode($username, $promo) {
         return User::wherePromoCode($promo)->whereUserName($username)->exists();
     }
-    protected function checkUser($username,$promo){
-
-        if ($this->userHasPromoCode($username, $promo)) {
-            return 'ok';
-        }
-        return abort(404);
-    }
 
     public function academiaDeApostas(Request $request)
     {
-        $this->checkUser($request->username,'866391');
+        if ($this->userHasPromoCode($request->username, '866391')) {
+            return 'ok';
+    }
+        return abort(404);
     }
 }
