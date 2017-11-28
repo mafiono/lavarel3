790b793f613a8fc0f30cbd02307ba287c26ceda7
José Miguel Couto <jose.couto@sfponline.pt>
2017-11-28 16:47:07 +0000

    #454 Adding new Regist System.

---
+++ resources/assets/js/common/components/register-form-step2.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="register-form-step2">
+        <register-form-title>Informação Legal</register-form-title>
+
+        <register-form-dropdown
+                name="nationality"
+                label="* Nacionalidade:"
+                :form="form"
+                :options="nationalities">
+            <option value="" disabled>Nacionalidade</option>
+            <option value="PT">Português</option>
+        </register-form-dropdown>
+
+        <register-form-textbox
+                name="document_number"
+                label="* Identificação Civil:"
+                :form="form"
+                placeholder="Identificação Civil">
+        </register-form-textbox>
+
+        <register-form-dropdown
+                name="sitprofession"
+                label="* Ocupação:"
+                :form="form">
+            <option value="" disabled>Ocupação</option>
+            <option value="11">Trabalhador por conta própria</option>
+            <option value="22">Trabalhador por conta de outrem</option>
+            <option value="33">Profissional liberal</option>
+            <option value="44">Estudante</option>
+            <option value="55">Reformado</option>
+            <option value="66">Estagiário</option>
+            <option value="77">Sem atividade profissional</option>
+            <option value="88">Desempregado</option>
+        </register-form-dropdown>
+
+        <register-form-textbox
+                name="tax_number"
+                label="* Número Fiscal:"
+                :form="form"
+                placeholder="Número Fiscal">
+        </register-form-textbox>
+
+        <register-form-textbox
+                name="bank_name"
+                label="Nome do Banco:"
+                :form="form"
+                placeholder="Nome do Banco">
+        </register-form-textbox>
+
+        <register-form-textbox
+                name="bank_bic"
+                label="BIC/SWIFT:"
+                :form="form"
+                placeholder="BIC/SWIFT">
+        </register-form-textbox>
+
+        <register-form-textbox
+                name="bank_iban"
+                label="IBAN:"
+                :form="form"
+                placeholder="IBAN">
+        </register-form-textbox>
+
+        <register-form-submit @submit="onSubmit" text="CONTINUAR 2/4" :disabled="submitDisabled"></register-form-submit>
+    </div>
+</template>
+
+<script>
+    import nationalities from './../js/nationalities';
+    import registerFormStepMixin from './mixins/register-form-step';
+
+    export default {
+        data() {
+            return {
+                nationalities
+            }
+        },
+        mixins: [registerFormStepMixin],
+        components: {
+            'register-form-title': require('./register-form-title.vue'),
+            'register-form-textbox': require('./register-form-textbox.vue'),
+            'register-form-dropdown': require('./register-form-dropdown.vue'),
+            'register-form-submit': require('./register-form-submit.vue')
+        }
+    }
+</script>
---
+++ resources/assets/js/common/js/registerRules.js
@@ -0,0 +1,103 @@
+export default {
+    firstname: [
+        {required: {message: 'Preencha o seu primeiro nome.'}}
+    ],
+    name: [
+        {required: {message: 'Preencha os seus apelidos.'}}
+    ],
+    birth_date: [
+        {required: {message: 'Selecione a sua data de nascimento.'}},
+        {date: {message: 'Data inválida.'}}
+    ],
+    email: [
+        {toggleValidation: {field: 'conf_email'}},
+        {required: {message: 'Preencha o seu email.'}},
+        {email: {message: 'Formato inválido'}},
+        {remote: {url: '/api/check-users', requestType: 'post', successMessage: 'true'}}
+    ],
+    conf_email: [
+        {equalTo: {message: 'Os emails não correspondem.', field: 'email'}}
+    ],
+
+    nationality: [
+        {required: {message: 'Selecione a sua nacionalidade.'}}
+    ],
+    document_number: [
+        {required: {message: 'Preencha o seu nº de identificação.'}},
+        {minLength: {message: 'Mínimo de 6 caracteres.', minLength: 6}},
+        {maxLength: {message: 'Máximo de 10 caracteres.', maxLength: 10}},
+        {remote: {url: '/api/check-users', requestType: 'post', successMessage: 'true'}}
+    ],
+    sitprofession: [
+        {required: {message: 'Selecione a sua situação profissional.'}}
+    ],
+    tax_number: [
+        {required: {message: 'Preencha o seu nº fiscal.'}},
+        {digits: {message: 'Só são permitidos dígitos.'}},
+        {minLength: {message: 'Mínimo de 6 caracteres.', minLength: 6}},
+        {maxLength: {message: 'Máximo de 10 caracteres.', maxLength: 10}},
+        {remote: {url: '/api/check-users', requestType: 'post', successMessage: 'true'}}
+    ],
+    bank_name: [
+        {required_if: {message: 'Preencha o nome do seu banco.', field: 'bank_iban'}},
+        {minLength: {message: 'Mínimo de 3 caracteres.', minLength: 3}},
+    ],
+    bank_bic: [
+        {toggleValidation: {field: 'bank_name'}},
+        {required: {message: 'Introduza um BIC/SWIFT.'}}
+    ],
+    bank_iban: [
+        {toggleValidation: {field: 'bank_name'}},
+        {iban: {message: 'Introduza um IBAN válido.'}}
+    ],
+    country: [
+        {required: {message: 'Preencha o seu país.'}}
+    ],
+    address: [
+        {required: {message: 'Preencha a sua morada.'}},
+        {minLength: {message: 'Mínimo de 6 caracteres.', minLength: 10}},
+        {maxLength: {message: 'Máximo de 10 caracteres.', maxLength: 150}}
+    ],
+    city: [
+        {required: {message: 'Preencha a sua cidade.'}},
+        {minLength: {message: 'Mínimo de 6 caracteres.', minLength: 4}},
+        {maxLength: {message: 'Máximo de 10 caracteres.', maxLength: 100}}
+
+    ],
+    district: [
+        {required: {message: 'Preencha o seu distrito.'}}
+    ],
+    zip_code: [
+        {required: {message: 'Preencha o seu código postal.'}},
+        {zipCode: {message: 'Formato: xxxx-xxx.'}}
+    ],
+    phone: [
+        {required: {message: 'Preencha o seu telefone.'}},
+        {phone: {message: 'Formato: +351 xxxxxxxxx.'}}
+    ],
+    username: [
+        {required: {message: 'Preencha o seu utilizador.'}},
+        {minLength: {message: 'Mínimo de 5 caracteres.', minLength: 5}},
+        {maxLength: {message: 'Máximo de 45 caracteres.', maxLength: 45}},
+        {username: {message: 'Formato inválido.'}},
+        {remote: {url: '/api/check-users', requestType: 'post', successMessage: 'true'}}
+    ],
+    password: [
+        {required: {message: 'Preencha a sua palavra passe.'}},
+        {minLength: {message: 'Mínimo de 8 caracteres.', minLength: 8}},
+        {maxLength: {message: 'Máximo de 20 caracteres.', maxLength: 20}},
+        {password: {message: '1 maiúscula, 1 minúscula e 1 numero.'}},
+        {toggleValidation: {field: 'conf_password'}}
+    ],
+    conf_password: [
+        {equalTo: {message: 'As palavras passe não correspondem.', field: 'password'}}
+    ],
+    captcha: [
+        {required: {message: 'Introduza o código do captcha.'}},
+        {minLength: {message: 'Mínimo de 5 caracteres.', minLength: 5}},
+        {maxLength: {message: 'Máximo de 5 caracteres.', maxLength: 5}}
+    ],
+    general_conditions: [
+        {is: {message: 'Por favor confirme que aceita os Termos e Condições.', value: true}}
+    ]
+}
