d8cb046b9917610c34c87d56781c9ace738471bd
Diogo.Araujo <diogo.araujo@sfponline.pt>
2018-01-24 15:04:33 +0000

    #462 add traduction

---
+++ resources/lang/en/golo_ouro.php
@@ -0,0 +1,15 @@
+<?php
+return[
+    'this_week' => 'This week win',
+    'scorer' =>'Who scored, when, and the correct score.',
+    'firstscorer' => '1st Scorer',
+    'minute' => 'Minute',
+    'result' => 'Result',
+    'amount' => 'Amount',
+    'bet_text' => 'Make your selection and win',
+    'odds' => 'Odds:',
+    'bet' => 'Bet',
+    'wait' => 'Waiting...',
+    'last_result' => 'Last Result:'
+]
+?>
