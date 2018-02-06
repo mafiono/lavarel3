ce51231a818fa741e1a0e9653b80c896162de3ae
Diogo.Araujo <diogo.araujo@sfponline.pt>
2018-02-06 11:15:08 +0000

    #462 add migration + layout

--- resources/assets/js/common/components/golodeouro.vue
+++ resources/assets/js/common/components/golodeouro.vue
@@ -195,7 +195,7 @@
             submit()
             {
                 $.post( "/golodeouro/aposta", {marcador:this.marcador,minuto:this.minuto,resultado:this.resultado,valor:this.valor,id:$('#id').val()})
-                    .done(this.submitDone(this.data));
+                    .done(function(data){alert(data)});
 
             },
             submitDone(data)
