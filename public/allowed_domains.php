bcac71c01dc1d5927a65c422968d0af2cd4f58b9
rafael.costa <rafael.costa@sfponline.pt>
2018-02-27 10:43:26 +0000

    #496: adding domains do the allowed list

--- public/allowed_domains.php
+++ public/allowed_domains.php
@@ -7,6 +7,9 @@ $domains = [
     'cdn.jsdelivr.net',
     'casinoportugal-static-test.casinomodule.com',
     'casinoportugal.pt',
+    'sandbox.meowallet.pt',
     'meowallet.pt',
+    'sandbox.paypal.com',
+    'paypal.com'
 ];
 header("Content-Security-Policy: frame-ancestors; script-src 'self' 'unsafe-eval' 'unsafe-inline' " . implode(' ', $domains) . "; object-src 'self'");
