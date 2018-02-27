13cb9b448e241059e53f814e030a3c6a0ed7778d
rafael.costa <rafael.costa@sfponline.pt>
2018-02-27 12:37:07 +0000

    #496: adding domains do the allowed list

--- public/allowed_domains.php
+++ public/allowed_domains.php
@@ -1,15 +1,31 @@
 <?php
 // Include trace Domains
 $domains = [
-//    'www.google-analytics.com',
-//    'embed.tawk.to',
-////    'wchat.freshchat.com',
-//    'cdn.jsdelivr.net',
-//    'casinoportugal-static-test.casinomodule.com',
-//    'casinoportugal.pt',
+    'www.google-analytics.com',
+    '*.tawk.to',
+    'wss://*.tawk.to',
+    '*.casinoportugal.pt',
+    'casinoportugal-static-test.casinomodule.com',
+    'stage-game-launcher-lux.isoftbet.com/',
+    'cdn.jsdelivr.net',
+    'fonts.googleapis.com',
+    'fonts.gstatic.com',
+    '*.switchpayments.com',
+    'stats.g.doubleclick.net',
+    'www.google.com/ads*',
+    'www.google.pt/ads*',
+    'casinoportugal.betstream.betgenius.com/',
+    'www.score24.com',
+    '*.paysafecard.com',
+//    'wchat.freshchat.com',
 //    'sandbox.meowallet.pt',
 //    'wallet.pt',
 //    'sandbox.paypal.com',
 //    'paypal.com',
 ];
-header("Content-Security-Policy: frame-ancestors; script-src 'self' 'unsafe-eval' 'unsafe-inline' " . implode(' ', $domains) . "; object-src 'self'");
+$parts = [
+    "default-src 'self' 'unsafe-eval' 'unsafe-inline' " . implode(' ', $domains),
+//    "connect-src wss://*.tawk.to",
+    "object-src 'self'",
+];
+header("Content-Security-Policy: frame-ancestors; " . implode('; ', $parts));
