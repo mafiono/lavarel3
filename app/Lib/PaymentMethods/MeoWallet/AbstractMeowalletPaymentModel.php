9d14f21303fc0cd43f62df501f415f17c84aafca
José Miguel Couto <jose.couto@sfponline.pt>
2018-03-13 15:58:50 +0000

    #516 Improve Meo wallet processing system

--- app/Lib/PaymentMethods/MeoWallet/AbstractMeowalletPaymentModel.php
+++ app/Lib/PaymentMethods/MeoWallet/AbstractMeowalletPaymentModel.php
@@ -19,13 +19,14 @@ class AbstractMeowalletPaymentModel
     protected $force = false;
     protected $applyCost = false;
 
-	const SANDBOX_ENVIRONMENT_ID       = 'sandbox';
-	const SANDBOX_SERVICE_ENDPOINT 	   = 'https://services.sandbox.meowallet.pt/api/v2';
-	const PRODUCTION_ENVIRONMENT_ID    = 'production';
-    const PRODUCTION_SERVICE_ENDPOINT  = 'https://services.wallet.pt/api/v2';
+	protected const SANDBOX_ENVIRONMENT_ID       = 'sandbox';
+	protected const SANDBOX_SERVICE_ENDPOINT 	 = 'https://services.sandbox.meowallet.pt/api/v2';
+	protected const PRODUCTION_ENVIRONMENT_ID    = 'production';
+    protected const PRODUCTION_SERVICE_ENDPOINT  = 'https://services.wallet.pt/api/v2';
 
     protected $_configs;
     protected $logger;
+    private $taxes;
 
     public function __construct($configs)
     {
@@ -117,128 +118,6 @@ class AbstractMeowalletPaymentModel
         throw new Exception($msg);
     }
 
-    public function processPayment($transaction_id, $invoice_id, $status, $amount, $method, $details = null)
-    {
-        $this->logger->info(sprintf("Processing payment for invoice_id '%s' with status '%s', amount '%s', trans_id: '%s', method: '%s'", $invoice_id, $status,
-            $amount, $transaction_id, $method));
-
-        // Check type of invoice
-        if ($uniqueRef = (strpos($invoice_id, '_') > 0)) {
-            list($user_id, $version) = explode('_', $invoice_id);
-            $userDep = UserDepositReference::query()
-                ->where('user_id', '=', $user_id)
-                ->where('version', '=', $version)
-                ->first()
-                ;
-            if ($userDep === null) {
-                throw new ModelNotFoundException("UserDepositReference: $invoice_id");
-            }
-            $taxes = TransactionTax::query()
-                ->where('transaction_id', '=', 'mb')
-                ->where('method', '=', 'deposit')
-                ->first()
-                ;
-            if ($taxes === null) {
-                throw new ModelNotFoundException('TransactionTax: mb -> deposit');
-            }
-            $trans = collect([]);
-            $user = $userDep->user;
-        } else {
-            /** @var UserTransaction[] $trans */
-            $trans = collect(UserTransaction::where('transaction_id', '=', $invoice_id)->get());
-            if (count($trans) === 0)
-                throw new ModelNotFoundException('Transaction');
-            /** @var User $user */
-            $fT = $trans->first();
-            $user = $fT->user;
-            $tax = $fT->tax;
-            $amountDeposit = $fT->debit;
-        }
-
-        $operations = $this->http('operations/?ext_invoiceid=' . $invoice_id);
-//        print_r('Found ' . $operations->total . PHP_EOL);
-        foreach ($operations->elements as $op) {
-            if (strtolower($op->method) === 'wallet') {
-                $op->method = 'meo_wallet';
-            }
-            $tran = $trans->first(function ($key, $item) use ($op) {
-                $match = $item->api_transaction_id === $op->id;
-                if ($match) return true;
-                if ($item->transaction_details === null) return false;
-                $json = json_decode($item->transaction_details);
-                if (isset($json->payment->id)) {
-                    return $json->payment->id === $op->id;
-                }
-                return false;
-            });
-            $details = json_encode($op);
-            if ($uniqueRef) {
-                $tran = UserTransaction::query()
-                    ->where('user_id', '=', $user_id)
-                    ->where('api_transaction_id', '=', $op->id)
-                    ->first();
-
-                $amountDeposit = (float)$op->amount;
-                $tax = 0;
-                if ($amountDeposit < $taxes->free_above) {
-                    $amountDeposit = round($amountDeposit / (1 + ($taxes->tax/100)), 2);
-                    $tax = (float)$op->amount - $amountDeposit;
-                }
-            }
-//            print_r('Found ' . $op->id . PHP_EOL);
-            if ($tran === null) {
-                $tran = $user->newDeposit($amountDeposit, strtolower($op->method), $tax, $op->id);
-                if (!$uniqueRef) {
-                    $tran->transaction_id = $invoice_id;
-                    $descTrans = Transaction::findOrNew(strtolower($op->method));
-                    $tran->description = 'Depósito ' . $descTrans->name . ' ' . $invoice_id;
-                    $tran->save();
-                }
-            }
-            if ($op->id !== $tran->api_transaction_id) {
-                // we can update the api_transaction_id (this can be the operation_id
-                $tran->api_transaction_id = $op->id;
-                $tran->save();
-            }
-            if (strtolower($op->method) !== $tran->origin) {
-                // we can update the method type
-                $tran->origin = strtolower($op->method);
-                $tran->save();
-            }
-            if ($this->applyCost && $op->amount !== ($tran->debit + $tran->tax)) {
-//                print_r('Removing Cost ' . ($tran->debit + $tran->credit + $tran->tax) . ' != ' . $op->amount . PHP_EOL);
-                $tran->debit = $op->amount - $tran->tax;
-                $tran->save();
-            }
-            if ($uniqueRef) {
-                $invoice_id = $tran->transaction_id;
-            }
-            switch ($op->status)
-            {
-                case 'COMPLETED':
-//                    print_r('Processing ' . $op->id . PHP_EOL);
-                    $result = $user->updateTransaction($invoice_id, $op->amount, 'processed', $tran->user_session_id,
-                        $op->id, $details, $op->fee, $this->force);
-                    $msg = sprintf("Processing payment for invoice_id: %s, id %s, result %s",
-                        $invoice_id, $op->id, $result);
-                    $this->logger->info($msg);
-                    print_r($msg . PHP_EOL);
-                    break;
-
-                case 'FAIL':
-                    // nothing to do here
-                    break;
-
-                case 'CREATED':
-                case 'PENDING':
-                    break;
-
-                default:
-                    throw new \InvalidArgumentException("Payment operation status '$status' not handled by this module!");
-            }
-        }
-    }
-
     public function processCallback($verbatim_callback)
     {
         if (false === $this->_isValidCallback($verbatim_callback))
@@ -296,5 +175,160 @@ class AbstractMeowalletPaymentModel
     public function applyCost($cost)
     {
         $this->applyCost = $cost;
+    }
+
+    public function processPayment($transaction_id, $invoice_id, $status, $amount, $method, $details = null)
+    {
+        $this->logger->info(sprintf("Processing payment for invoice_id '%s' with status '%s', amount '%s', trans_id: '%s', method: '%s'", $invoice_id, $status,
+            $amount, $transaction_id, $method));
+
+        $operations = $this->http('operations/?limit=30&ext_invoiceid=' . $invoice_id);
+        print_r('Found ' . $operations->total . PHP_EOL);
+        foreach ($operations->elements as $op) {
+            $this->processSingleOperation($op);
+        }
+    }
+
+    /**
+     * @param $op
+     */
+    public function processSingleOperation($op)
+    {
+        if ($op->type !== 'PAYMENT')
+            throw new Exception("Unknown Operation type $op->type");
+        if (strtolower($op->method) === 'wallet') {
+            $op->method = 'meo_wallet';
+        }
+        $invoice_id = $op->ext_invoiceid;
+        $uniqueRef = strpos($invoice_id, '_') > 0;
+        $details = json_encode($op);
+        /** @var User $user */
+        if ($uniqueRef) {
+            $userDep = $this->getUniqueReference($invoice_id);
+            $user = $userDep->user;
+            $tran = UserTransaction::query()
+                ->where('user_id', '=', $userDep->user_id)
+                ->where('api_transaction_id', '=', $op->id)
+                ->first();
+
+            $amountDeposit = (float)$op->amount;
+            $tax = 0;
+            $taxes = $this->getMbTaxes();
+            if ($amountDeposit < $taxes->free_above) {
+                $amountDeposit = round($amountDeposit / (1 + ($taxes->tax / 100)), 2);
+                $tax = (float)$op->amount - $amountDeposit;
+            }
+        } else {
+            $trans = $this->getTransactionsForInvoice($invoice_id);
+            /** @var UserTransaction $tran */
+            $tran = $trans->first(function ($key, $item) use ($op) {
+                $match = $item->api_transaction_id === $op->id;
+                if ($match) return true;
+                if ($item->transaction_details === null) return false;
+                $json = json_decode($item->transaction_details);
+                if (isset($json->payment->id)) {
+                    return $json->payment->id === $op->id;
+                }
+                return false;
+            });
+            if ($tran === null) {
+                throw new ModelNotFoundException('Transaction not found: ' . $invoice_id);
+            }
+            $user = $tran->user;
+        }
+//            print_r('Found ' . $op->id . PHP_EOL);
+        if ($tran === null) {
+            $tran = $user->newDeposit($amountDeposit, strtolower($op->method), $tax, $op->id);
+            if (!$uniqueRef) {
+                $tran->transaction_id = $invoice_id;
+                $descTrans = Transaction::findOrNew(strtolower($op->method));
+                $tran->description = 'Depósito ' . $descTrans->name . ' ' . $invoice_id;
+                $tran->save();
+            }
+        }
+        if ($op->id !== $tran->api_transaction_id) {
+            // we can update the api_transaction_id (this can be the operation_id
+            $tran->api_transaction_id = $op->id;
+            $tran->save();
+        }
+        if (strtolower($op->method) !== $tran->origin) {
+            // we can update the method type
+            $tran->origin = strtolower($op->method);
+            $tran->save();
+        }
+        if ($this->applyCost && $op->amount !== ($tran->debit + $tran->tax)) {
+//                print_r('Removing Cost ' . ($tran->debit + $tran->credit + $tran->tax) . ' != ' . $op->amount . PHP_EOL);
+            $tran->debit = $op->amount - $tran->tax;
+            $tran->save();
+        }
+        if ($uniqueRef) {
+            $invoice_id = $tran->transaction_id;
+        }
+        switch ($op->status) {
+            case 'COMPLETED':
+//                    print_r('Processing ' . $op->id . PHP_EOL);
+                $result = $user->updateTransaction($invoice_id, $op->amount, 'processed', $tran->user_session_id,
+                    $op->id, $details, $op->fee, $this->force);
+                $msg = sprintf('Processing payment User: %s Invoice_id: %s, OpId: %s, result: %s',
+                    $user->id, $invoice_id, $op->id, $result === false ? 'FAIL' : $result);
+                $this->logger->info($msg);
+                print_r($msg . PHP_EOL);
+                break;
+
+            case 'FAIL':
+                // nothing to do here
+                break;
+
+            case 'CREATED':
+            case 'PENDING':
+                break;
+
+            default:
+                throw new \InvalidArgumentException("Payment operation status '$op->status' not handled by this module!");
+        }
+    }
+
+    /**
+     * @return TransactionTax
+     */
+    private function getMbTaxes()
+    {
+        if ($this->taxes !== null)
+            return $this->taxes;
+        $this->taxes = TransactionTax::query()
+            ->where('transaction_id', '=', 'mb')
+            ->where('method', '=', 'deposit')
+            ->first();
+        if ($this->taxes === null) {
+            throw new ModelNotFoundException('TransactionTax: mb -> deposit');
+        }
+        return $this->taxes;
+    }
+
+    private function getTransactionsForInvoice($invoice_id)
+    {
+        $trans = collect(UserTransaction::where('transaction_id', '=', $invoice_id)->get());
+        if (count($trans) === 0)
+            throw new ModelNotFoundException('Transaction for invoice:' . $invoice_id);
+
+        return $trans;
+    }
+
+    /**
+     * @param $invoice_id
+     * @return UserDepositReference
+     */
+    private function getUniqueReference($invoice_id)
+    {
+        list($user_id, $version) = explode('_', $invoice_id);
+        $userDep = UserDepositReference::query()
+            ->where('user_id', '=', $user_id)
+            ->where('version', '=', $version)
+            ->first()
+        ;
+        if ($userDep === null) {
+            throw new ModelNotFoundException("UserDepositReference: $invoice_id");
+        }
+        return $userDep;
     }
 }
