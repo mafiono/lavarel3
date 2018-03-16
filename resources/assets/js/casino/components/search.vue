503f175c66d7d1b71792098c2d2d3d129d300d23
rafael.costa <rafael.costa@sfponline.pt>
2018-03-16 16:21:52 +0000

    #483: Impementation of the api on the search

--- resources/assets/js/casino/components/search.vue
+++ resources/assets/js/casino/components/search.vue
@@ -1,39 +1,44 @@
 <template>
     <form id="searchForm" @submit.prevent="updateSearch()">
-        <input id="textSearch" v-model="query" type="text" class="botao-registar brand-back" placeholder="Procurar">
+        <input id="textSearch" :value="query" @input="updateSearch"
+               type="text" class="botao-registar brand-back" placeholder="Procurar">
     </form>
 </template>
 <script>
     import Store from '../../common/store/store';
+    import {Subject} from 'rxjs/Subject';
+    import 'rxjs/add/operator/filter';
+    import 'rxjs/add/operator/debounceTime';
+
+    let active$ = new Subject();
+    active$.next(true);
 
     export default {
         data: function() {
             return this.$root.$data.search;
         },
         methods: {
-            updateSearch: function() {
-                if (this.query.length < 1) return;
-
-                Store.games.searchGames(this.query)
-                    .then(games => {
-                        if (games.length > 0 || Store.mobile.isMobile) {
-                            this.games = games;
-                            this.$router.push('/pesquisa/' + this.query);
-                        }
-                    })
-                ;
-            }
-        },
-        watch: {
-            query: function() {
-                this.updateSearch();
+            updateSearch: function (event) {
+                this.query = event.target.value;
+                this.search$.next(this.query);
             }
         },
         mounted: function() {
+            this.search$ = new Subject();
+            this.search$
+                .debounceTime(400)
+                .filter(x => x.length > 0)
+                .do(x => this.$router.push('/pesquisa/' + x))
+                .switchMap(x => Store.games.searchGames(x))
+                .filter(games => games.length || Store.mobile.isMobile)
+                .subscribe(games => this.games = games);
+
             if (this.$root.$route.path.includes('/pesquisa')
                 && Object.keys(this.$root.$route.params).length
-                && this.$root.$route.params.term)
+                && this.$root.$route.params.term) {
                 this.query = this.$root.$route.params.term;
+                this.search$.next(this.query);
+            }
         }
     }
 </script>
