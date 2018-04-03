2085bde449c5abfc8aa0e4acc38247c21ffaf987
José Miguel Couto <jose.couto@sfponline.pt>
2018-04-03 11:41:56 +0100

    #522 Using the router to change Css class.

--- resources/assets/js/common/components/mobile-left-menu-header.vue
+++ resources/assets/js/common/components/mobile-left-menu-header.vue
@@ -2,19 +2,19 @@
     <div class="mobile-left-menu-header">
         <ul>
             <li style="width:25%">
-                <a href="/" :class="selectedCss('sports')" @click="defineContext('sports')">DESPORTO</a>
+                <a href="/" :class="selectedCss('sports')">DESPORTO</a>
             </li>
             <li style="width:25%">
-                <a href="/golodeouro" :class="selectedCss('golodeouro')" @click="defineContext('golodeouro')">GOLO D'OURO</a>
+                <a href="/golodeouro" :class="selectedCss('golodeouro')">GOLO D'OURO</a>
             </li>
             <li style="width:25%">
                 <router-link to="/">
-                    <a href="javascript:;" :class="selectedCss('casino')" @click="OpenCasino();defineContext('casino')">CASINO</a>
+                    <a href="javascript:;" :class="selectedCss('casino')" @click="OpenCasino()">CASINO</a>
                 </router-link>
             </li>
             <li style="width:25%">
                 <router-link to="/promocoes">
-                    <a href="/promocoes" :class="selectedCss('promotions')" @click="defineContext('promotions')">PROMOÇÕES</a>
+                    <a href="/promocoes" :class="selectedCss('promocoes')">PROMOÇÕES</a>
                 </router-link>
             </li>
         </ul>
@@ -24,30 +24,25 @@
 <script>
     export default {
         props: ['context'],
-        data(){
-            return {local: this.context}
+        data() {
+            return Store.app
         },
         methods: {
             selectedCss: function (link) {
-                var context;
-                if (this.local === 'casino') {
-                    context = 'casino';
-                } else if (this.local === 'golodeouro') {
-                    context = 'golodeouro';
-                } else if (this.local === 'promotions'){
-                    context = 'promotions';
-                } else {
-                    context = 'sports';
+                switch (link) {
+                    case 'promocoes': return this.currentRoute === '/promocoes' ? 'selected' : '';
+                    case 'golodeouro': return this.currentRoute === '/golodeouro' ? 'selected' : '';
+                    case 'casino': return this.context === 'casino' && this.currentRoute !== '/promocoes' ? 'selected' : '';
+                    case 'sports':
+                    default: return this.currentRoute !== '/promocoes'
+                        && this.currentRoute !== '/golodeouro'
+                        && this.context !== 'casino' ? 'selected' : '';
                 }
-                return context === link ? 'selected' : '';
             },
             OpenCasino() {
                 if (this.context !== 'casino') {
                     window.location = '/casino';
                 }
-            },
-            defineContext(value){
-                return this.local = value;
             }
         },
     }
