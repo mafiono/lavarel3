a6b88e1359cbcaa2528b59bfb6b974604f4fa43e
Diogo.Araujo <diogo.araujo@sfponline.pt>
2018-05-17 11:42:21 +0100

    #558

--- app/Http/Controllers/GoloDeOuroController.php
+++ app/Http/Controllers/GoloDeOuroController.php
@@ -8,6 +8,7 @@ use App\Bets\Validators\BetslipBetValidator;
 use App\Http\Traits\GenericResponseTrait;
 use App\Models\Golodeouro;
 use App\Models\GolodeouroSelection;
+use App\Models\GolodeouroValue;
 use App\UserBetEvent;
 use App\UserSession;
 use Carbon\Carbon;
@@ -59,62 +60,65 @@ class GoloDeOuroController extends Controller
         $bet->result = 'waiting_result';
         $bet->status = 'waiting_result';
         $bet->user_session_id = UserSession::getSessionId();
-        $golodeouro = Golodeouro::find($inputs['id']);
-        $bet->odd = $golodeouro->odd;
+        $bet->odd = $golo->odd;
         $bet->type = 'multi';
         $bet->cp_fixture_id = $inputs['id'];
 
-
-        if (BetslipBetValidator::make($bet)->validate()) {
-
-            $selectionmarcador = GolodeouroSelection::find($inputs['marcador']);
-            $selectionminuto = GolodeouroSelection::find($inputs['minuto']);
-            $selectionresultado = GolodeouroSelection::find($inputs['resultado']);
-
-            if($selectionminuto === null || $selectionmarcador === null || $selectionresultado === null)
-            {
-                return response('Error', 400);
-            }
-            BetBookie::placeBet($bet);
-
-            $eventmarcador = new UserBetEvent;
-            $eventmarcador->user_bet_id = $bet->id;
-            $eventmarcador->odd = $selectionmarcador->odd;
-            $eventmarcador->status = 'waiting_result';
-            $eventmarcador->event_name = $golodeouro->fixture->name;
-            $eventmarcador->market_name = 'Primeiro Marcador';
-            $eventmarcador->game_name = 'golodeouro';
-            $eventmarcador->game_date = $golo->fixture->start_time_utc;
-            $eventmarcador->api_event_id = $selectionmarcador->id;
-            $eventmarcador->api_game_id = $golo->fixture->id;
-
-            $eventminuto = new UserBetEvent;
-            $eventminuto->user_bet_id = $bet->id;
-            $eventminuto->odd = $selectionminuto->odd;
-            $eventminuto->status = 'waiting_result';
-            $eventminuto->event_name = $golodeouro->fixture->name;
-            $eventminuto->market_name = 'Minuto Primeiro Golo';
-            $eventminuto->game_name = 'golodeouro';
-            $eventminuto->game_date = $golo->fixture->start_time_utc;
-            $eventminuto->api_event_id = $selectionminuto->id;
-            $eventminuto->api_game_id = $golo->fixture->id;
-
-            $eventresultado = new UserBetEvent;
-            $eventresultado->user_bet_id = $bet->id;
-            $eventresultado->odd = $selectionresultado->odd;
-            $eventresultado->status = 'waiting_result';
-            $eventresultado->event_name = $golodeouro->fixture->name;
-            $eventresultado->market_name = 'Resultado Correcto';
-            $eventresultado->game_name = 'golodeouro';
-            $eventresultado->game_date = $golo->fixture->start_time_utc;
-            $eventresultado->api_event_id = $selectionresultado->id;
-            $eventresultado->api_game_id = $golo->fixture->id;
-
-            $eventmarcador->save();
-            $eventminuto->save();
-            $eventresultado->save();
-
+        if(!GolodeouroValue::where('cp_fixture_id',$golo->id)->where('amount',$inputs['valor'])->exists())
+        {
+            return response('Error', 400);
         }
+
+        if (!BetslipBetValidator::make($bet)->validate()) {
+            return response('Error',400);
+        }
+        $selectionmarcador = GolodeouroSelection::find($inputs['marcador']);
+        $selectionminuto = GolodeouroSelection::find($inputs['minuto']);
+        $selectionresultado = GolodeouroSelection::find($inputs['resultado']);
+
+        if($selectionminuto === null || $selectionmarcador === null || $selectionresultado === null)
+        {
+            return response('Error', 400);
+        }
+        BetBookie::placeBet($bet);
+
+        $eventmarcador = new UserBetEvent;
+        $eventmarcador->user_bet_id = $bet->id;
+        $eventmarcador->odd = $selectionmarcador->odd;
+        $eventmarcador->status = 'waiting_result';
+        $eventmarcador->event_name = $selectionmarcador->name;
+        $eventmarcador->market_name = 'Primeiro Marcador';
+        $eventmarcador->game_name = $golo->fixture->name;
+        $eventmarcador->game_date = $golo->fixture->start_time_utc;
+        $eventmarcador->api_event_id = $selectionmarcador->id;
+        $eventmarcador->api_game_id = $golo->fixture->id;
+
+        $eventminuto = new UserBetEvent;
+        $eventminuto->user_bet_id = $bet->id;
+        $eventminuto->odd = $selectionminuto->odd;
+        $eventminuto->status = 'waiting_result';
+        $eventminuto->event_name = $selectionminuto->name;
+        $eventminuto->market_name = 'Minuto Primeiro Golo';
+        $eventminuto->game_name = $golo->fixture->name;
+        $eventminuto->game_date = $golo->fixture->start_time_utc;
+        $eventminuto->api_event_id = $selectionminuto->id;
+        $eventminuto->api_game_id = $golo->fixture->id;
+
+        $eventresultado = new UserBetEvent;
+        $eventresultado->user_bet_id = $bet->id;
+        $eventresultado->odd = $selectionresultado->odd;
+        $eventresultado->status = 'waiting_result';
+        $eventresultado->event_name = $selectionresultado->name;
+        $eventresultado->market_name = 'Resultado Correcto';
+        $eventresultado->game_name = $golo->fixture->name;
+        $eventresultado->game_date = $golo->fixture->start_time_utc;
+        $eventresultado->api_event_id = $selectionresultado->id;
+        $eventresultado->api_game_id = $golo->fixture->id;
+
+        $eventmarcador->save();
+        $eventminuto->save();
+        $eventresultado->save();
+
         return response('Success', 200);
     }
 
