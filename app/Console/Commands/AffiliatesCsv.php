b005233e019a8c7b1ae67075802a74f65252aeb0
Diogo.Araujo <diogo.araujo@sfponline.pt>
2018-05-24 15:31:58 +0100

    #557 merge master

--- app/Console/Commands/AffiliatesCsv.php
+++ app/Console/Commands/AffiliatesCsv.php
@@ -202,14 +202,14 @@ class AffiliatesCsv extends Command
         }
         fclose($outreg);
 
-//        if (FTP::connection('ftp_afiliados')->uploadFile($pathReg, '/' . $nameReg))
-//            $this->line("Colocado $nameReg no FTP com sucesso!!");
-//        else
-//            $this->line("Erro ao colocar o $nameReg no FTP!");
-//        if (FTP::connection('ftp_afiliados')->uploadFile($pathSales, '/' . $nameSales))
-//            $this->line("Colocado $nameSales no FTP com sucesso!!");
-//        else
-//            $this->line("Erro ao colocar o $nameSales no FTP!");
+        if (FTP::connection('ftp_afiliados')->uploadFile($pathReg, '/' . $nameReg))
+            $this->line("Colocado $nameReg no FTP com sucesso!!");
+        else
+            $this->line("Erro ao colocar o $nameReg no FTP!");
+        if (FTP::connection('ftp_afiliados')->uploadFile($pathSales, '/' . $nameSales))
+            $this->line("Colocado $nameSales no FTP com sucesso!!");
+        else
+            $this->line("Erro ao colocar o $nameSales no FTP!");
 
     }
 }
